e55df26e60ab8a689c030e3f2cb5cfcf576d5584
Aryatama <18211338+aryatama045@users.noreply.github.com>
2024-02-02 11:25:53 +0700

    Update head-scripts.blade.php

--- resources/views/frontend/skinoasis/inc/head-scripts.blade.php
+++ resources/views/frontend/skinoasis/inc/head-scripts.blade.php
@@ -4,8 +4,7 @@
         };
         (function(d) {
             var wf = d.createElement('script'), s = d.scripts[0];
-            var url = {{ staticAsset('frontend/skinoasis') }};
-            wf.src = url + '/assets/js/webfont.js';
+            wf.src = "{{ staticAsset('frontend/skinoasis/assets/js/webfont.js') }}";
             wf.async = true;
             s.parentNode.insertBefore(wf, s);
         })(document);
