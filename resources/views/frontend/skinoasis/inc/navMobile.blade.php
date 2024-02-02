268909b699e000b9623c6f638321a76e18e894b3
Aryatama <18211338+aryatama045@users.noreply.github.com>
2024-02-02 18:27:40 +0700

    Update navMobile.blade.php

--- resources/views/frontend/skinoasis/inc/navMobile.blade.php
+++ resources/views/frontend/skinoasis/inc/navMobile.blade.php
@@ -9,9 +9,25 @@
 
             <nav class="mobile-nav">
                 <ul class="mobile-menu">
-                    <li class="active">
-                        <a href="index.html">Home</a>
-                    </li>
+                        @if (!is_null(getSetting('header_menu_labels')))
+                            @php
+                                $labels = json_decode(getSetting('header_menu_labels')) ?? [];
+                                $menus = json_decode(getSetting('header_menu_links')) ?? [];
+                            @endphp
+
+                            @foreach ($menus as $menuKey => $menuItem)
+                                <li class="<?php if($menuItem==localize($labels[$menuKey])){ "active"; } ?>" >
+                                    <a href="{{ $menuItem }}">{{ localize($labels[$menuKey]) }}</a>
+                                </li>
+                            @endforeach
+                        @else
+                            <li><a href="{{ route('home') }}">{{ localize('Home') }}</a></li>
+                            <li><a href="{{ route('products.index') }}">{{ localize('Products') }}</a></li>
+                            <li><a href="{{ route('home.campaigns') }}">{{ localize('Campaigns') }}</a>
+                            </li>
+                            <li><a href="{{ route('home.coupons') }}">{{ localize('Coupons') }}</a>
+                            </li>
+                        @endif
                 </ul>
             </nav><!-- End .mobile-nav -->
 
