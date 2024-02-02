91ef6196278c12984d03cf1265cc47fc0a39dc5a
Aryatama <18211338+aryatama045@users.noreply.github.com>
2024-02-02 16:23:24 +0700

    up

--- resources/views/frontend/skinoasis/inc/header.blade.php
+++ resources/views/frontend/skinoasis/inc/header.blade.php
@@ -68,7 +68,7 @@
                     </a>
 
                     <div class="dropdown-menu dropdown-menu-right">
-                        <div class="dropdown-cart-products ">
+                        <div class="dropdown-cart-products simplebar-content">
                             <!--cart listing-->
                             @include('frontend.skinoasis.pages.partials.carts.cart-navbar', [
                                     'carts' => $carts, ])
