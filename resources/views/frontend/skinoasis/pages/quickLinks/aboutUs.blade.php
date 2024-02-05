807dfaf585550cc240fe60079fc6029091542921
Aryatama <18211338+aryatama045@users.noreply.github.com>
2024-02-05 14:06:49 +0700

    Update aboutUs.blade.php

--- resources/views/frontend/skinoasis/pages/quickLinks/aboutUs.blade.php
+++ resources/views/frontend/skinoasis/pages/quickLinks/aboutUs.blade.php
@@ -19,7 +19,7 @@
             <div class="col-lg-10 offset-lg-1">
                 <div class="about-text mt-3">
                     <h2 class="title text-center mb-2"> SKINOASIS</h2><!-- End .title text-center mb-2 -->
-                    <p align="text-left">Skinoasis is a special platform which provides beauty solutions, packed with great knowledge and integrity to become an oasis for everyone who is looking for excellent beauty products. Established in 2023 with years' experience in the beauty industry. Skinoasis also a collaborative platform to bring together an experts, doctors, scientist, products and beloved customer for discovering  a great beauty experience.</p>
+                    <p align="justify">Skinoasis is a special platform which provides beauty solutions, packed with great knowledge and integrity to become an oasis for everyone who is looking for excellent beauty products. Established in 2023 with years' experience in the beauty industry. Skinoasis also a collaborative platform to bring together an experts, doctors, scientist, products and beloved customer for discovering  a great beauty experience.</p>
                     <img src="assets/images/about/about-2/signature.png" alt="signature" class="mx-auto mb-5">
 
                     <img src="assets/images/about/about-2/img-1.jpg" alt="image" class="mx-auto mb-6">
