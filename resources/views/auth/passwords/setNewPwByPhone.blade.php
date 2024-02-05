7ba6a3714dd3f39b667f7f4ce4cdf7cec73ccb16
Aryatama <18211338+aryatama045@users.noreply.github.com>
2024-02-05 15:32:15 +0700

    update logo

--- resources/views/auth/passwords/setNewPwByPhone.blade.php
+++ resources/views/auth/passwords/setNewPwByPhone.blade.php
@@ -17,7 +17,7 @@
                         @csrf
                         <div class="mb-7">
                             <a href="{{ route('home') }}">
-                                <img src="{{ uploadedAsset(getSetting('navbar_logo')) }}" alt="logo">
+                            <img src="{{ uploadedAsset(getSetting('admin_panel_logo')) }}" alt="logo" width="250">
                             </a>
                         </div>
                         <h2 class="mb-4 h3">{{ localize('Reset Password') }}
