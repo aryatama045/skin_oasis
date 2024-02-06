1052aa651ebad8b678bd877d092b85b2c9c78f89
Aryatama <18211338+aryatama045@users.noreply.github.com>
2024-02-06 14:34:59 +0700

    Update show.blade.php

--- resources/views/frontend/skinoasis/pages/products/show.blade.php
+++ resources/views/frontend/skinoasis/pages/products/show.blade.php
@@ -221,43 +221,47 @@
             <div class="tab-content">
                 <div class="tab-pane fade show active" id="product-desc-tab" role="tabpanel" aria-labelledby="product-desc-link">
                     <div class="product-desc-content">
-                        <h3>Product Information</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna viverra non, semper suscipit, posuere a, pede. Donec nec justo eget felis facilisis fermentum. Aliquam porttitor mauris sit amet orci. Aenean dignissim pellentesque felis. Phasellus ultrices nulla quis nibh. Quisque a lectus. Donec consectetuer ligula vulputate sem tristique cursus. </p>
-                        <ul>
-                            <li>Nunc nec porttitor turpis. In eu risus enim. In vitae mollis elit. </li>
-                            <li>Vivamus finibus vel mauris ut vehicula.</li>
-                            <li>Nullam a magna porttitor, dictum risus nec, faucibus sapien.</li>
-                        </ul>
-
-                        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna viverra non, semper suscipit, posuere a, pede. Donec nec justo eget felis facilisis fermentum. Aliquam porttitor mauris sit amet orci. Aenean dignissim pellentesque felis. Phasellus ultrices nulla quis nibh. Quisque a lectus. Donec consectetuer ligula vulputate sem tristique cursus. </p>
+                    @if ($product->description)
+                        {!! $product->collectLocalization('description') !!}
+                    @else
+                        <div class="text-dark text-center border py-2">{{ localize('Not Available') }}
+                        </div>
+                    @endif
                     </div><!-- End .product-desc-content -->
                 </div><!-- .End .tab-pane -->
                 <div class="tab-pane fade" id="product-info-tab" role="tabpanel" aria-labelledby="product-info-link">
                     <div class="product-desc-content">
-                        <h3>Information</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna viverra non, semper suscipit, posuere a, pede. Donec nec justo eget felis facilisis fermentum. Aliquam porttitor mauris sit amet orci. </p>
-
-                        <h3>Fabric & care</h3>
-                        <ul>
-                            <li>Faux suede fabric</li>
-                            <li>Gold tone metal hoop handles.</li>
-                            <li>RI branding</li>
-                            <li>Snake print trim interior </li>
-                            <li>Adjustable cross body strap</li>
-                            <li> Height: 31cm; Width: 32cm; Depth: 12cm; Handle Drop: 61cm</li>
-                        </ul>
-
-                        <h3>Size</h3>
-                        <p>one size</p>
+                        <h6 class="mb-2">{{ localize('Additional Information') }}:</h6>
+                        <table class="w-100 product-info-table">
+                            @forelse (generateVariationOptions($product->variation_combinations) as $variation)
+                            <tr>
+                                <td class="text-dark fw-semibold">{{ $variation['name'] }}</td>
+                                <td>
+                                    @foreach ($variation['values'] as $value)
+                                        {{ $value['name'] }}@if (!$loop->last)
+                                            ,
+                                        @endif
+                                    @endforeach
+                                </td>
+                            </tr>
+                            @empty
+                                <tr>
+                                    <td class="text-dark text-center" colspan="2">{{ localize('Not Available') }}
+                                    </td>
+                                </tr>
+                            @endforelse
+                        </table>
                     </div><!-- End .product-desc-content -->
                 </div><!-- .End .tab-pane -->
-                <div class="tab-pane fade" id="product-shipping-tab" role="tabpanel" aria-labelledby="product-shipping-link">
+
+                <!-- .<div class="tab-pane fade" id="product-shipping-tab" role="tabpanel" aria-labelledby="product-shipping-link">
                     <div class="product-desc-content">
                         <h3>Delivery & returns</h3>
                         <p>We deliver to over 100 countries around the world. For full details of the delivery options we offer, please view our <a href="#">Delivery information</a><br>
                         We hope you’ll love every purchase, but if you ever need to return an item you can do so within a month of receipt. For full details of how to make a return, please view our <a href="#">Returns information</a></p>
-                    </div><!-- End .product-desc-content -->
-                </div><!-- .End .tab-pane -->
+                    </div>
+                </div>End .tab-pane -->
+
                 <div class="tab-pane fade" id="product-review-tab" role="tabpanel" aria-labelledby="product-review-link">
                     <div class="reviews">
                         <h3>Reviews (2)</h3>
