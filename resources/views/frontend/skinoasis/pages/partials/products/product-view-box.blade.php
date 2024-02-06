b08651e4ac6a45736cdbca23fcd8e4c7b0dee167
Aryatama <18211338+aryatama045@users.noreply.github.com>
2024-02-06 13:43:45 +0700

    update show

--- resources/views/frontend/skinoasis/pages/partials/products/product-view-box.blade.php
+++ resources/views/frontend/skinoasis/pages/partials/products/product-view-box.blade.php
@@ -1,49 +1,102 @@
-<div class="product product-7 text-center">
-    <figure class="product-media">
+<div class="gstore-product-quick-view bg-white rounded-3 py-6 px-4">
+    <div class="row g-4">
+        <div class="col-xl-6 align-self-end">
+            <!-- sliders -->
+            @include('frontend.default.pages.partials.products.sliders', compact('product'))
+            <!-- sliders -->
+        </div>
+        <div class="col-xl-6">
+            <div class="product-info">
+                <h3 class="mt-1 mb-3 h5">{{ $product->collectLocalization('name') }}</h3>
 
-        <span class="product-label label-new">New</span>
+                <!-- pricing -->
+                <div class="pricing all-pricing mt-2">
+                    @include('frontend.default.pages.partials.products.pricing', compact('product'))
+                </div>
+                <!-- pricing -->
 
-        <a href="product.html">
-            <img src="assets/images/products/product-4.jpg" alt="Product image" class="product-image">
-        </a>
+                <!-- selected variation pricing -->
+                <div class="pricing variation-pricing mt-2 d-none">
 
-        <div class="product-action-vertical">
+                </div>
+                <!-- selected variation pricing -->
 
-            <a href="#" class="btn-product-icon btn-wishlist btn-expandable"><span>add to wishlist</span></a>
+                <div class="widget-title d-flex mt-4">
+                    <h6 class="mb-1 flex-shrink-0">{{ localize('Description') }}</h6>
+                    <span class="hr-line w-100 position-relative d-block align-self-end ms-1"></span>
+                </div>
+                <p class="mb-3">
+                    {{ $product->collectLocalization('short_description') }}
+                </p>
 
-            <a href="popup/quickView.html" class="btn-product-icon btn-quickview" title="Quick view"><span>Quick view</span></a>
-            <a href="#" class="btn-product-icon btn-compare" title="Compare"><span>Compare</span></a>
-        </div><!-- End .product-action-vertical -->
+                <form action="" class="add-to-cart-form">
+                    @php
+                        $isVariantProduct = 0;
+                        $stock = 0;
+                        if ($product->variations()->count() > 1) {
+                            $isVariantProduct = 1;
+                        } else {
+                            $stock = $product->variations[0]->product_variation_stock ? $product->variations[0]->product_variation_stock->stock_qty : 0;
+                        }
+                    @endphp
 
-        <div class="product-action">
-            <a href="#" class="btn-product btn-cart"><span>add to cart</span></a>
-        </div><!-- End .product-action -->
+                    <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                    <input type="hidden" name="product_variation_id"
+                        @if (!$isVariantProduct) value="{{ $product->variations[0]->id }}" @endif>
 
-    </figure><!-- End .product-media -->
+                    <!-- variations -->
+                    @include('frontend.default.pages.partials.products.variations', compact('product'))
+                    <!-- variations -->
 
-    <div class="product-body">
+                    <div class="d-flex align-items-center gap-3 flex-wrap mt-5">
+                        <div class="product-qty qty-increase-decrease d-flex align-items-center">
+                            <button type="button" class="decrease">-</button>
+                            <input type="text" readonly value="1" name="quantity" min="1"
+                                @if (!$isVariantProduct) max="{{ $stock }}" @endif>
+                            <button type="button" class="increase">+</button>
+                        </div>
 
-        <!--product category start-->
-        @if ($product->categories()->count() > 0)
-            <div class="product-cat">
-                @foreach ($product->categories as $category)
-                    <a href="{{ route('products.index') }}?&category_id={{ $category->id }}"
-                        class="text-muted fs-xxs">{{ $category->collectLocalization('name') }}</a>
-                @endforeach
+                        <button type="submit" class="btn btn-secondary btn-md add-to-cart-btn"
+                            @if (!$isVariantProduct && $stock < 1) disabled @endif>
+                            <span class="me-2">
+                                <i class="fa-solid fa-bag-shopping"></i>
+                            </span>
+                            <span class="add-to-cart-text">
+                                @if (!$isVariantProduct && $stock < 1)
+                                    {{ localize('Out of Stock') }}
+                                @else
+                                    {{ localize('Add to Cart') }}
+                                @endif
+                            </span>
+                        </button>
+
+                        <button type="button" class="btn btn-primary btn-md"
+                            onclick="addToWishlist({{ $product->id }})">
+                            <i class="fa-solid fa-heart"></i>
+                        </button>
+
+                        <div class="flex-grow-1"></div>
+                        @if (getSetting('enable_reward_points') == 1)
+                            <span class="fw-bold" data-bs-toggle="tooltip" data-bs-placement="top"
+                                data-bs-title="{{ localize('Reward Points') }}">
+                                <i class="fas fa-medal"></i> {{ $product->reward_points }}
+                            </span>
+                        @endif
+                    </div>
+
+                    <!--product category start-->
+                    @if ($product->categories()->count() > 0)
+                        <div class="tt-category-tag mt-4">
+                            @foreach ($product->categories as $category)
+                                <a href="{{ route('products.index') }}?&category_id={{ $category->id }}"
+                                    class="text-muted fs-xxs">{{ $category->collectLocalization('name') }}</a>
+                            @endforeach
+                        </div>
+                    @endif
+                    <!--product category end-->
+                </form>
+
             </div>
-        @endif
-        <!-- End .product-cat -->
-
-
-        <h3 class="product-title">
-            <a href="product.html">
-                {{ $product->collectLocalization('name') }}
-            </a>
-        </h3><!-- End .product-title -->
-        
-        <div class="product-price">
-            @include('frontend.default.pages.partials.products.pricing', compact('product'))
-        </div><!-- End .product-price -->
-
-    </div><!-- End .product-body -->
-</div><!-- End .product -->
+        </div>
+    </div>
+</div>
--- resources/views/frontend/skinoasis/pages/products/show.blade.php
+++ resources/views/frontend/skinoasis/pages/products/show.blade.php
@@ -78,35 +78,19 @@
         <div class="product-details-top">
             <div class="row">
                 <div class="col-md-6">
-                    <div class="product-gallery product-gallery-vertical">
-                        <div class="row">
-                            <figure class="product-main-image">
-                                <img id="product-zoom" src="{{ staticAsset('frontend/skinoasis/assets/images/products/single/1.jpg') }}" data-zoom-image="{{ staticAsset('frontend/skinoasis/assets/images/products/single/1-big.jpg') }}" alt="product image">
+                    <div class="product-details">
+                        <!-- product-view-box -->
+                        @include(
+                            'frontend.default.pages.partials.products.product-view-box',
+                            compact('product'))
+                        <!-- product-view-box -->
 
-                                <a href="#" id="btn-product-gallery" class="btn-product-gallery">
-                                    <i class="icon-arrows"></i>
-                                </a>
-                            </figure><!-- End .product-main-image -->
-
-                            <div id="product-zoom-gallery" class="product-image-gallery">
-                                <a class="product-gallery-item active" href="#" data-image="{{ staticAsset('frontend/skinoasis/assets/images/products/single/1.jpg') }}" data-zoom-image="{{ staticAsset('frontend/skinoasis/assets/images/products/single/1-big.jpg') }}">
-                                    <img src="{{ staticAsset('frontend/skinoasis/assets/images/products/single/1-small.jpg') }}" alt="product side">
-                                </a>
-
-                                <a class="product-gallery-item" href="#" data-image="{{ staticAsset('frontend/skinoasis/assets/images/products/single/2.jpg') }}" data-zoom-image="{{ staticAsset('frontend/skinoasis/assets/images/products/single/2-big.jpg') }}">
-                                    <img src="{{ staticAsset('frontend/skinoasis/assets/images/products/single/2-small.jpg') }}" alt="product cross">
-                                </a>
-
-                                <a class="product-gallery-item" href="#" data-image="{{ staticAsset('frontend/skinoasis/assets/images/products/single/3.jpg') }}" data-zoom-image="{{ staticAsset('frontend/skinoasis/assets/images/products/single/3-big.jpg') }}">
-                                    <img src="{{ staticAsset('frontend/skinoasis/assets/images/products/single/3-small.jpg') }}" alt="product with model">
-                                </a>
-
-                                <a class="product-gallery-item" href="#" data-image="assets/images/products/single/4.jpg" data-zoom-image="{{ staticAsset('frontend/skinoasis/assets/images/products/single/4-big.jpg') }}">
-                                    <img src="{{ staticAsset('frontend/skinoasis/assets/images/products/single/4-small.jpg') }}" alt="product back">
-                                </a>
-                            </div><!-- End .product-image-gallery -->
-                        </div><!-- End .row -->
-                    </div><!-- End .product-gallery -->
+                        <!-- description -->
+                        @include(
+                            'frontend.default.pages.partials.products.description',
+                            compact('product'))
+                        <!-- description -->
+                    </div>
                 </div><!-- End .col-md-6 -->
 
                 <div class="col-md-6">
