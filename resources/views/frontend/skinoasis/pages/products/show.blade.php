de02b38de6e7d864499d9006709b639044387b1d
Aryatama <18211338+aryatama045@users.noreply.github.com>
2024-02-06 14:11:32 +0700

    Update show.blade.php

--- resources/views/frontend/skinoasis/pages/products/show.blade.php
+++ resources/views/frontend/skinoasis/pages/products/show.blade.php
@@ -134,31 +134,33 @@
                                     <!-- variations -->
 
                                     <div class="d-flex align-items-center gap-3 flex-wrap mt-5">
-                                        <div class="product-qty qty-increase-decrease d-flex align-items-center">
-                                            <button type="button" class="decrease">-</button>
-                                            <input type="text" readonly value="1" name="quantity" min="1"
-                                                @if (!$isVariantProduct) max="{{ $stock }}" @endif>
-                                            <button type="button" class="increase">+</button>
+                                        <div class="product-details-quantity">
+                                            <div class="product-qty qty-increase-decrease d-flex align-items-center">
+                                                <button type="button" class="decrease">-</button>
+                                                <input type="text" readonly value="1" name="quantity" min="1"
+                                                    @if (!$isVariantProduct) max="{{ $stock }}" @endif>
+                                                <button type="button" class="increase">+</button>
+                                            </div>
                                         </div>
 
-                                        <button type="submit" class="btn btn-secondary btn-md add-to-cart-btn"
-                                            @if (!$isVariantProduct && $stock < 1) disabled @endif>
-                                            <span class="me-2">
-                                                <i class="fa-solid fa-bag-shopping"></i>
-                                            </span>
-                                            <span class="add-to-cart-text">
-                                                @if (!$isVariantProduct && $stock < 1)
-                                                    {{ localize('Out of Stock') }}
-                                                @else
-                                                    {{ localize('Add to Cart') }}
-                                                @endif
-                                            </span>
-                                        </button>
+                                        <div class="product-details-action">
+                                            <button type="submit" class="btn-product btn-cart add-to-cart-btn"
+                                                @if (!$isVariantProduct && $stock < 1) disabled @endif>
 
-                                        <button type="button" class="btn btn-primary btn-md"
-                                            onclick="addToWishlist({{ $product->id }})">
-                                            <i class="fa-solid fa-heart"></i>
-                                        </button>
+                                                <span class="add-to-cart-text">
+                                                    @if (!$isVariantProduct && $stock < 1)
+                                                        {{ localize('Out of Stock') }}
+                                                    @else
+                                                        {{ localize('Add to Cart') }}
+                                                    @endif
+                                                </span>
+                                            </button>
+
+                                            <button type="button" class="btn btn-primary btn-md"
+                                                onclick="addToWishlist({{ $product->id }})">
+                                                <i class="fa-solid fa-heart"></i>
+                                            </button>
+                                        </div>
 
                                         <div class="flex-grow-1"></div>
                                         @if (getSetting('enable_reward_points') == 1)
