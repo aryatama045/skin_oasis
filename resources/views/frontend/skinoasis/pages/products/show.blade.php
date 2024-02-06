0573766a87a0147e145d1f91b0ab8bdfe13ea4ad
Aryatama <18211338+aryatama045@users.noreply.github.com>
2024-02-06 13:40:44 +0700

    Update show.blade.php

--- resources/views/frontend/skinoasis/pages/products/show.blade.php
+++ resources/views/frontend/skinoasis/pages/products/show.blade.php
@@ -62,7 +62,6 @@
 @endsection
 
 @section('contents')
-    
 
     <div class="page-header text-center" style="background-image: url('{{ staticAsset('frontend/skinoasis/assets/images/page-header-bg.jpg') }}')">
         <div class="container">
