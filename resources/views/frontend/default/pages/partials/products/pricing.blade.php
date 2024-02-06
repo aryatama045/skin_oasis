fcf3d48629e52fd9d9b9cae4bcfccb8da1cbab17
Aryatama <18211338+aryatama045@users.noreply.github.com>
2024-02-06 14:24:18 +0700

    Update pricing.blade.php

--- resources/views/frontend/default/pages/partials/products/pricing.blade.php
+++ resources/views/frontend/default/pages/partials/products/pricing.blade.php
@@ -2,17 +2,14 @@
     @if (productBasePrice($product) == productMaxPrice($product))
         <h3 class="fw-bold text-danger">{{ formatPrice(productBasePrice($product)) }}</h3>
     @else
-        <h3 class="fw-bold text-danger">{{ formatPrice(productBasePrice($product)) }}</h3>
-        -
-        <h3 class="fw-bold text-danger">{{ formatPrice(productMaxPrice($product)) }}</h3>
+        <h3 class="fw-bold text-danger">{{ formatPrice(productBasePrice($product)) }} - {{ formatPrice(productMaxPrice($product)) }}</h3>
+
     @endif
 @else
     @if (discountedProductBasePrice($product) == discountedProductMaxPrice($product))
         <h3 class="fw-bold text-danger">{{ formatPrice(discountedProductBasePrice($product)) }}</h3>
     @else
-        <h3 class="fw-bold text-danger">{{ formatPrice(discountedProductBasePrice($product)) }}</h3>
-        -
-        <h3 class="fw-bold text-danger">{{ formatPrice(discountedProductMaxPrice($product)) }}</h3>
+        <h3 class="fw-bold text-danger">{{ formatPrice(discountedProductBasePrice($product)) }} - {{ formatPrice(discountedProductMaxPrice($product)) }}</h3>
     @endif
 
     @if (isset($br))
@@ -24,10 +21,8 @@
             <h3
                 class="fw-bold deleted text-muted {{ isset($br) ? '' : 'ms-1' }}">{{ formatPrice(productBasePrice($product)) }}</h3>
         @else
-            <h3
-                class="fw-bold deleted text-muted {{ isset($br) ? '' : 'ms-1' }}">{{ formatPrice(productBasePrice($product)) }}</h3>
-            -
-            <h3 class="fw-bold deleted text-muted ms-1">{{ formatPrice(productMaxPrice($product)) }}</h3>
+            <h3 class="fw-bold deleted text-muted {{ isset($br) ? '' : 'ms-1' }}">{{ formatPrice(productBasePrice($product)) }} - {{ formatPrice(productMaxPrice($product)) }}</h3>
+
         @endif
     @endif
 @endif
