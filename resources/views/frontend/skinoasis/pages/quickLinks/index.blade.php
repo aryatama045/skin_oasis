44ae93daa579849af71135f9ba51b381fe1c6d83
Aryatama <18211338+aryatama045@users.noreply.github.com>
2024-02-07 11:15:01 +0700

    Update index.blade.php

--- resources/views/frontend/skinoasis/pages/quickLinks/index.blade.php
+++ resources/views/frontend/skinoasis/pages/quickLinks/index.blade.php
@@ -5,19 +5,20 @@
 @endsection
 
 @section('breadcrumb-contents')
-    <div class="breadcrumb-content">
-        <h2 class="mb-2 text-center">{{ $page->collectLocalization('title') }}</h2>
-        <nav>
-            <ol class="breadcrumb justify-content-center">
-                <li class="breadcrumb-item fw-bold" aria-current="page"><a
-                        href="{{ route('home') }}">{{ localize('Home') }}</a></li>
-                <li class="breadcrumb-item fw-bold" aria-current="page">{{ localize('Quick Links') }}</li>
-            </ol>
-        </nav>
-    </div>
+    <ol class="breadcrumb ">
+        <li class="breadcrumb-item fw-bold" aria-current="page"><a
+                href="{{ route('home') }}">{{ localize('Home') }}</a></li>
+        <li class="breadcrumb-item fw-bold" aria-current="page">{{ localize('Quick Links') }}</li>
+    </ol>
 @endsection
 
 @section('contents')
+
+    <!--pageheader-->
+    @include('frontend.skinoasis.inc.pageHeader',
+            ['title'=> getSetting('system_title')])
+    <!--pageheader-->
+
     <!--breadcrumb-->
     @include('frontend.skinoasis.inc.breadcrumb')
     <!--breadcrumb-->
