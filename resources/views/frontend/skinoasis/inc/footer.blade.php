93cca1748cc775838fa69578fba1d18982409a8a
Aryatama <18211338+aryatama045@users.noreply.github.com>
2024-02-07 13:10:09 +0700

    Update footer.blade.php

--- resources/views/frontend/skinoasis/inc/footer.blade.php
+++ resources/views/frontend/skinoasis/inc/footer.blade.php
@@ -65,7 +65,7 @@
             </ul><!-- End .footer-menu -->
 
             <div class="social-icons social-icons-color">
-                <span class="social-label">Social Media</span>
+                <!-- <span class="social-label">Social Media</span> -->
                 <a href="#" class="social-icon social-facebook" title="Facebook" target="_blank"><i class="icon-facebook-f"></i></a>
                 <a href="#" class="social-icon social-twitter" title="Twitter" target="_blank"><i class="icon-twitter"></i></a>
                 <a href="#" class="social-icon social-instagram" title="Instagram" target="_blank"><i class="icon-instagram"></i></a>
