e247cfb4a78502cc4913fca711048446f557ecab
Aryatama <18211338+aryatama045@users.noreply.github.com>
2024-02-07 16:13:50 +0700

    update Box

--- resources/views/frontend/skinoasis/pages/halloBeauty/inc/2info.blade.php
+++ resources/views/frontend/skinoasis/pages/halloBeauty/inc/2info.blade.php
@@ -1,20 +1,20 @@
-<div class="icon-boxes-container bg-yellow">
+<div class="icon-boxes-hello-beauty">
     <div class="container">
         <div class="row">
-            <div class="col-sm-6 col-lg-3">
-                <div class="icon-box icon-box-side">
+            <div class="col-sm-4 col-lg-4">
+                <div class="icon-box-hb icon-box-side">
                     <span class="icon-box-icon text-dark">
                         <i class="icon-rocket"></i>
                     </span>
                     <div class="icon-box-content">
-                        <h3 class="icon-box-title">Free Shipping</h3><!-- End .icon-box-title -->
-                        <p>orders $50 or more</p>
+                        <h3 class="icon-box-title">KLINIK KECANTIKAN <br> DI SELURUH INDONESIA</h3><!-- End .icon-box-title -->
+                        <!-- <p>orders $50 or more</p> -->
                     </div><!-- End .icon-box-content -->
                 </div><!-- End .icon-box -->
-            </div><!-- End .col-sm-6 col-lg-3 -->
+            </div><!-- End .col-sm-4 col-lg-4 -->
 
-            <div class="col-sm-6 col-lg-3">
-                <div class="icon-box icon-box-side">
+            <div class="col-sm-4 col-lg-4">
+                <div class="icon-box-hb icon-box-side">
                     <span class="icon-box-icon text-dark">
                         <i class="icon-rotate-left"></i>
                     </span>
@@ -24,10 +24,10 @@
                         <p>within 30 days</p>
                     </div><!-- End .icon-box-content -->
                 </div><!-- End .icon-box -->
-            </div><!-- End .col-sm-6 col-lg-3 -->
+            </div><!-- End .col-sm-4 col-lg-4 -->
 
-            <div class="col-sm-6 col-lg-3">
-                <div class="icon-box icon-box-side">
+            <div class="col-sm-4 col-lg-4">
+                <div class="icon-box-hb icon-box-side">
                     <span class="icon-box-icon text-dark">
                         <i class="icon-info-circle"></i>
                     </span>
@@ -37,20 +37,8 @@
                         <p>When you sign up</p>
                     </div><!-- End .icon-box-content -->
                 </div><!-- End .icon-box -->
-            </div><!-- End .col-sm-6 col-lg-3 -->
+            </div><!-- End .col-sm-4 col-lg-4 -->
 
-            <div class="col-sm-6 col-lg-3">
-                <div class="icon-box icon-box-side">
-                    <span class="icon-box-icon text-dark">
-                        <i class="icon-life-ring"></i>
-                    </span>
-
-                    <div class="icon-box-content">
-                        <h3 class="icon-box-title">We Support</h3><!-- End .icon-box-title -->
-                        <p>24/7 amazing services</p>
-                    </div><!-- End .icon-box-content -->
-                </div><!-- End .icon-box -->
-            </div><!-- End .col-sm-6 col-lg-3 -->
         </div><!-- End .row -->
     </div><!-- End .container -->
 </div><!-- End .icon-boxes-container -->
