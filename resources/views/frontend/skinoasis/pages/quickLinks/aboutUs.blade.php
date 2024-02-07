d84e5e974c764ba17e9a171c42738b8e61eea407
Aryatama <18211338+aryatama045@users.noreply.github.com>
2024-02-07 11:16:13 +0700

    Update aboutUs.blade.php

--- resources/views/frontend/skinoasis/pages/quickLinks/aboutUs.blade.php
+++ resources/views/frontend/skinoasis/pages/quickLinks/aboutUs.blade.php
@@ -20,26 +20,29 @@
                 <div class="about-text-header ">
                     <h2 class="title text-center mb-2"> SKINOASIS</h2><!-- End .title text-center mb-2 -->
                     <p align="justify">Skinoasis is a special platform which provides beauty solutions, packed with great knowledge and integrity to become an oasis for everyone who is looking for excellent beauty products. Established in 2023 with years' experience in the beauty industry. Skinoasis also a collaborative platform to bring together an experts, doctors, scientist, products and beloved customer for discovering  a great beauty experience.</p>
-                    <img src="assets/images/about/about-2/signature.png" alt="signature" class="mx-auto mb-5">
-
-                    <img src="assets/images/about/about-2/img-1.jpg" alt="image" class="mx-auto mb-6">
                 </div><!-- End .about-text -->
             </div><!-- End .col-lg-10 offset-1 -->
         </div><!-- End .row -->
         <div class="row justify-content-center">
-            <div class="col-lg-4 col-sm-6">
-                <div class="icon-box icon-box-sm text-center">
-                    <span class="icon-box-icon">
-                        <i class="icon-puzzle-piece"></i>
-                    </span>
-                    <div class="icon-box-content">
-                        <h3 class="icon-box-title">Design Quality</h3><!-- End .icon-box-title -->
-                        <p>Sed egestas, ante et vulputate volutpat, eros pede semper est, vitae luctus metus libero <br>eu augue.</p>
-                    </div><!-- End .icon-box-content -->
-                </div><!-- End .icon-box -->
-            </div><!-- End .col-lg-4 col-sm-6 -->
+            <div class="col-lg-6 col-sm-12">
+                <div class="clearfix">
+                    <img src="{{ staticAsset('frontend/skinoasis/assets/images/banner1.png') }}" class="col-md-6 float-md-end mb-3 ms-md-3" alt="...">
 
-            <div class="col-lg-4 col-sm-6">
+                    <p>
+                        A paragraph of placeholder text. We're using it here to show the use of the clearfix class. We're adding quite a few meaningless phrases here to demonstrate how the columns interact here with the floated image.
+                    </p>
+
+                    <p>
+                        As you can see the paragraphs gracefully wrap around the floated image. Now imagine how this would look with some actual content in here, rather than just this boring placeholder text that goes on and on, but actually conveys no tangible information at. It simply takes up space and should not really be read.
+                    </p>
+
+                    <p>
+                        And yet, here you are, still persevering in reading this placeholder text, hoping for some more insights, or some hidden easter egg of content. A joke, perhaps. Unfortunately, there's none of that here.
+                    </p>
+                </div>
+            </div><!-- End .col-lg-6 col-sm-12 -->
+
+            <div class="col-lg-6 col-sm-12">
                 <div class="icon-box icon-box-sm text-center">
                     <span class="icon-box-icon">
                         <i class="icon-life-ring"></i>
@@ -51,17 +54,7 @@
                 </div><!-- End .icon-box -->
             </div><!-- End .col-lg-4 col-sm-6 -->
 
-            <div class="col-lg-4 col-sm-6">
-                <div class="icon-box icon-box-sm text-center">
-                    <span class="icon-box-icon">
-                        <i class="icon-heart-o"></i>
-                    </span>
-                    <div class="icon-box-content">
-                        <h3 class="icon-box-title">Made With Love</h3><!-- End .icon-box-title -->
-                        <p>Pellentesque a diam sit amet mi ullamcorper <br>vehicula. Nullam quis massa sit amet <br>nibh viverra malesuada.</p> 
-                    </div><!-- End .icon-box-content -->
-                </div><!-- End .icon-box -->
-            </div><!-- End .col-lg-4 col-sm-6 -->
+
         </div><!-- End .row -->
     </div><!-- End .container -->
 
