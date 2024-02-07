e981e3f7c87e11ef69e4860ac08da33cde138268
Aryatama <18211338+aryatama045@users.noreply.github.com>
2024-02-07 10:27:47 +0700

    Update index.blade.php

--- resources/views/frontend/skinoasis/pages/blogs/index.blade.php
+++ resources/views/frontend/skinoasis/pages/blogs/index.blade.php
@@ -1,26 +1,21 @@
-@extends('frontend.default.layouts.master')
+@extends('frontend.skinoasis.layouts.master')
 
 @section('title')
     {{ localize('Blogs') }} {{ getSetting('title_separator') }} {{ getSetting('system_title') }}
 @endsection
 
 @section('breadcrumb-contents')
-    <div class="breadcrumb-content">
-        <h2 class="mb-2 text-center">{{ localize('Blogs') }}</h2>
-        <nav>
-            <ol class="breadcrumb justify-content-center">
-                <li class="breadcrumb-item fw-bold" aria-current="page"><a
-                        href="{{ route('home') }}">{{ localize('Home') }}</a></li>
-                <li class="breadcrumb-item fw-bold" aria-current="page"><a
-                        href="{{ route('home.blogs') }}">{{ localize('Blogs') }}</a></li>
-            </ol>
-        </nav>
-    </div>
+    <ol class="breadcrumb justify-content-center">
+        <li class="breadcrumb-item fw-bold" aria-current="page"><a
+                href="{{ route('home') }}">{{ localize('Home') }}</a></li>
+        <li class="breadcrumb-item fw-bold" aria-current="page"><a
+                href="{{ route('home.blogs') }}">{{ localize('Blogs') }}</a></li>
+    </ol>
 @endsection
 
 @section('contents')
     <!--breadcrumb-->
-    @include('frontend.default.inc.breadcrumb')
+    @include('frontend.skinoasis.inc.breadcrumb')
     <!--breadcrumb-->
 
     <!--blog details start-->
@@ -64,7 +59,7 @@
                     </div>
                 </div>
                 <div class="col-xl-4">
-                    @include('frontend.default.pages.partials.blogs.blogSidebar')
+                    @include('frontend.skinoasis.pages.partials.blogs.blogSidebar')
                 </div>
             </div>
 
