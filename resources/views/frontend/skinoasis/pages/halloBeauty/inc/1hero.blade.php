ad64e44e6c9c526a2cd07679f481ee1c46ef1bd9
NuryantoWahyudi <nuryantowahyudi8@gmail.com>
2024-02-07 15:40:46 +0700

    Hallo Beauty

--- resources/views/frontend/skinoasis/pages/halloBeauty/inc/1hero.blade.php
+++ resources/views/frontend/skinoasis/pages/halloBeauty/inc/1hero.blade.php
@@ -4,22 +4,7 @@
 
     @foreach ($sliders as $slider)
         @if($slider->display_on == 2)
-            <div class="intro-slide" style="background-image: url({{ uploadedAsset($slider->image) }});">
-                <div class="container">
-                    <div class="intro-content text-center">
-                        <h3 class="intro-subtitle cross-txt text-primary">{{ $slider->sub_title }}</h3><!-- End .h3 intro-subtitle -->
-                        <h1 class="intro-title text-white">{{ $slider->title }}</h1><!-- End .intro-title -->
-                        <div class="intro-text text-white">{{ $slider->text }}</div><!-- End .intro-text -->
-
-
-                        <div class="intro-action cross-txt">
-                            <a href="{{ $slider->link }}" class="btn btn-outline-white">
-                                <span>Discover More</span>
-                            </a>
-                        </div>
-                    </div>
-                </div><!-- End .intro-content -->
-            </div><!-- End .intro-slide -->
+            <div class="intro-slide" style="background-image: url({{ uploadedAsset($slider->image) }});"></div>
         @endif
     @endforeach
     </div><!-- End .intro-slider owl-carousel owl-simple -->
