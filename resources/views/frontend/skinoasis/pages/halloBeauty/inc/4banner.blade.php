4a956b1a6189b245a9348977de590baaa68432ad
Aryatama <18211338+aryatama045@users.noreply.github.com>
2024-02-09 15:32:40 +0700

    Update 4banner.blade.php

---
+++ resources/views/frontend/skinoasis/pages/halloBeauty/inc/4banner.blade.php
@@ -0,0 +1,11 @@
+<div class="banner-big" style="background-image: url({{ staticAsset('frontend/skinoasis/assets/images/demos/demo-31/banners/5.jpg') }}); background-color: #f1f5f7;">
+    <div class="banner-content">
+        <div class="banner-subtitle text-secondary">Find online Yoga and Meditation here.</div>
+        <div class="banner-title text-secondary mb-3">Everything For Your Yoga Practice</div>
+        <figure class="wave-img">
+            <img src="{{ staticAsset('frontend/skinoasis/assets/images/demos/demo-31/wave.png') }}" alt="Wave image" width="76" height="9">
+        </figure>
+        <p class="font-weight-normal mt-3">From mats, props, accessories, and everything in between, delivered direct to your door.</p>
+        <a href="#" class="btn btn-secondary">SHOP YOGA MATS</a>
+    </div><!--End .banner-content-->
+</div><!--End .banner-big-->
