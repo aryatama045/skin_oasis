1add19aab70593238ad90937d0a63085c0cb3c39
Aryatama <18211338+aryatama045@users.noreply.github.com>
2024-02-11 16:41:07 +0700

    Update
    -Checkout
    -Home section 6

--- resources/views/frontend/skinoasis/pages/checkout/carts.blade.php
+++ resources/views/frontend/skinoasis/pages/checkout/carts.blade.php
@@ -29,103 +29,95 @@
     <div class="page-content">
         <div class="cart">
             <div class="container">
-                <div class="row">
-                    <div class="col-lg-9">
+                <div class="rounded-2 overflow-hidden">
+                    <table class="cart-table w-100 bg-white">
+                        <thead>
+                            <th>{{ localize('Image') }}</th>
+                            <th>{{ localize('Product Name') }}</th>
+                            <th>{{ localize('U. Price') }}</th>
+                            <th>{{ localize('Quantity') }}</th>
+                            <th>{{ localize('T. Price') }}</th>
+                            <th>{{ localize('Action') }}</th>
+                        </thead>
+                        <tbody class="cart-listing">
+                            <!--cart listing-->
+                            @include('frontend.skinoasis.pages.partials.carts.cart-listing', ['carts' => $carts])
+                            <!--cart listing-->
+                        </tbody>
+                    </table>
+                </div>
+                <div class="row g-4">
+                    <div class="col-xl-7">
+                        <div class="voucher-box py-7 px-5 position-relative z-1 overflow-hidden bg-white rounded mt-4">
+                            <img src="{{ staticAsset('frontend/default/assets/img/shapes/circle-half.png') }}"
+                                alt="circle shape" class="position-absolute end-0 top-0 z--1">
+                            <h4 class="mb-4">{{ localize('Have a coupon?') }}</h4>
+                            <div class="font-bold mb-2">{{ localize('Apply coupon to get discount.') }}</div>
 
-                        <div class="rounded-2 overflow-hidden">
-                            <table class="cart-table table table-cart table-mobile w-100 bg-white">
-                                <thead>
-                                    <th>{{ localize('Image') }}</th>
-                                    <th>{{ localize('Product Name') }}</th>
-                                    <th>{{ localize('U. Price') }}</th>
-                                    <th>{{ localize('Quantity') }}</th>
-                                    <th>{{ localize('T. Price') }}</th>
-                                    <th>{{ localize('Action') }}</th>
-                                </thead>
-                                <tbody class="cart-listing">
-                                    <!--cart listing-->
-                                    @include('frontend.skinoasis.pages.partials.carts.cart-listing', ['carts' => $carts])
-                                    <!--cart listing-->
-                                </tbody>
-                            </table>
+                            <!-- coupon form -->
+                            <form class="d-flex align-items-center coupon-form">
+                                <input type="hidden" name="_token" value="{{ csrf_token() }}">
+
+                                <input type="text" name="code" placeholder="{{ localize('Enter Your Coupon Code') }}"
+                                    class="theme-input w-100 coupon-input"
+                                    @if (isset($_COOKIE['coupon_code'])) value="{{ $_COOKIE['coupon_code'] }}" disabled @endif
+                                    required>
+
+                                @if (isset($_COOKIE['coupon_code']))
+                                    <button type="submit"
+                                        class="btn btn-secondary flex-shrink-0 apply-coupon-btn d-none px-4">{{ localize('Apply Coupon') }}</button>
+                                    <button type="button" class="btn btn-secondary flex-shrink-0 clear-coupon-btn"><i
+                                            class="fas fa-close"></i></button>
+                                @else
+                                    <button type="submit"
+                                        class="btn btn-secondary flex-shrink-0 apply-coupon-btn px-4">{{ localize('Apply Coupon') }}</button>
+                                    <button type="button" class="btn btn-secondary flex-shrink-0 clear-coupon-btn d-none"><i
+                                            class="fas fa-close"></i></button>
+                                @endif
+                            </form>
+                            <!-- coupon form -->
+
                         </div>
+                    </div>
 
-                        <div class="cart-bottom">
-                            <div class="cart-discount">
-                                <form action="#" class="coupon-form">
-                                    <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                                    <div class="input-group">
-                                        <input type="text" class="form-control theme-input w-100 coupon-input"
-                                                name="code" placeholder="{{ localize('Enter Your Coupon Code') }}"
-                                                @if (isset($_COOKIE['coupon_code'])) value="{{ $_COOKIE['coupon_code'] }}" disabled @endif
-                                                required>
-                                        @if (isset($_COOKIE['coupon_code']))
-                                            <div class="input-group-append">
-                                                <button type="submit"
-                                                    class="btn btn-secondary btn-outline-primary-2 flex-shrink-0 apply-coupon-btn d-none px-4">{{ localize('Apply Coupon') }}</button>
-                                                <button type="button" class="btn btn-secondary btn-outline-primary-2 flex-shrink-0 clear-coupon-btn"><i
-                                                    class="fas fa-close"></i></button>
-                                            </div><!-- .End .input-group-append -->
-                                        @else
-                                            <div class="input-group-append">
-                                                <button type="submit"
-                                                    class="btn btn-secondary btn-outline-primary-2 flex-shrink-0 apply-coupon-btn px-4">{{ localize('Apply Coupon') }}</button>
-                                                <button type="button" class="btn btn-secondary btn-outline-primary-2 flex-shrink-0 clear-coupon-btn d-none"><i
-                                                        class="fas fa-close"></i></button>
-                                            </div><!-- .End .input-group-append -->
-                                        @endif
+                    <div class="col-xl-5">
+                        <div class="cart-summery bg-white rounded-2 pt-4 pb-7 px-5 mt-4">
+                            <table class="w-100">
+                                <tr>
+                                    <td class="py-3">
+                                        <h5 class="mb-0 fw-medium">{{ localize('Subtotal') }}</h5>
+                                    </td>
+                                    <td class="py-3">
+                                        <h5 class="mb-0 text-end sub-total-price">
+                                            {{ formatPrice(getSubTotal($carts, false)) }}</h5>
+                                    </td>
+                                </tr>
 
-                                    </div><!-- End .input-group -->
-                                </form>
-                            </div><!-- End .cart-discount -->
+                                <tr class="coupon-discount-wrapper {{ getCoupon() == '' ? 'd-none' : '' }}">
+                                    <td class="py-3">
+                                        <h5 class="mb-0 fw-medium">{{ localize('Coupon Discount') }}</h5>
+                                    </td>
+                                    <td class="py-3">
+                                        <h5 class="mb-0 text-end coupon-discount-price">
+                                            {{ formatPrice(getCouponDiscount(getSubTotal($carts, false), getCoupon())) }}</h5>
+                                    </td>
+                                </tr>
 
-                            <a href="#" class="btn btn-outline-dark-2"><span>UPDATE CART</span><i class="icon-refresh"></i></a>
-                        </div><!-- End .cart-bottom -->
-                    </div><!-- End .col-lg-9 -->
-
-                    <aside class="col-lg-3">
-                        <div class="summary summary-cart">
-                            <h3 class="summary-title">Cart Total</h3><!-- End .summary-title -->
-
-                            <table class="table table-summary">
-                                <tbody>
-                                    <tr class="summary-subtotal">
-                                        <td>{{ localize('Subtotal') }}</td>
-                                        <td><h5 class="mb-0 text-end sub-total-price">
-                                            {{ formatPrice(getSubTotal($carts, false)) }}</h5></td>
-                                    </tr><!-- End .summary-subtotal -->
-
-                                    <tr class="coupon-discount-wrapper {{ getCoupon() == '' ? 'd-none' : '' }}">
-                                        <td class="py-3">
-                                            <h5 class="mb-0 fw-medium">{{ localize('Coupon Discount') }}</h5>
-                                        </td>
-                                        <td class="py-3">
-                                            <h5 class="mb-0 text-end coupon-discount-price">
-                                                {{ formatPrice(getCouponDiscount(getSubTotal($carts, false), getCoupon())) }}</h5>
-                                        </td>
-                                    </tr>
-
-                                    <tr class="summary-total">
-                                        <td>Total:</td>
-                                        <td>$160.00</td>
-                                    </tr><!-- End .summary-total -->
-                                </tbody>
-                            </table><!-- End .table table-summary -->
-
+                            </table>
                             <p class="mb-5 mt-2">{{ localize('Shipping options will be updated during checkout.') }}</p>
+                            <div class="btns-group d-flex flex-wrap gap-3">
 
-                            <!-- Btn Checkout -->
-                            <a href="{{ route('checkout.proceed') }}" type="submit"
-                                class="btn btn-outline-primary-2 btn-order btn-block btn-md rounded-1">{{ localize('Checkout') }}</a>
+                                <a href="{{ route('home') }}"
+                                    class="btn btn-outline-secondary border-secondary btn-md rounded-1">{{ localize('Continue Shopping') }}</a>
 
-                        </div><!-- End .summary -->
+                                <a href="{{ route('checkout.proceed') }}" type="submit"
+                                    class="btn btn-primary btn-md rounded-1">{{ localize('Checkout') }}</a>
+                            </div>
+                        </div>
+                    </div>
 
-                        <!-- Btn Contunie -->
-                        <a href="{{ route('home') }}" class="btn btn-outline-dark-2 btn-block mb-3"><span> {{ localize('Continue Shopping') }}</span><i class="icon-refresh"></i></a>
-
-                    </aside><!-- End .col-lg-3 -->
-                </div><!-- End .row -->
-            </div><!-- End .container -->
+                </div>
+            </div>
         </div><!-- End .cart -->
     </div><!-- End .page-content -->
 
---
+++ resources/views/frontend/skinoasis/pages/partials/home/6bannerstwo.blade.php
@@ -0,0 +1,32 @@
+<section class="gallery pt-15 pb-15" style="background-image: url({{ staticAsset('frontend/skinoasis/assets/images/demos/demo-30/bg-3.png') }})">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="content-left">
+                    <div class="heading mt-3">
+                        <h3 class="">Material &amp; Design</h3>
+                        <h2 class="title mb-2">Product <br>Gallery</h2>
+                        <p class="content mb-6">
+                            Donec nec justo eget felis facilisis fermentum. Aliquam <br>
+                            porttitor mauris sit amet orci. Aenean dignissim <br>
+                            pellentesque felis.
+                        </p>
+                    </div>
+                    <figure>
+                        <img src="{{ staticAsset('frontend/skinoasis/assets/images/demos/demo-30/banners/banner-1.jpg') }}" alt="Banner">
+                    </figure>
+                </div>
+            </div>
+            <div class="col-md-6 p-md-0">
+                <div class="content-right">
+                    <figure class="mb-md-12">
+                        <img src="{{ staticAsset('frontend/skinoasis/assets/images/demos/demo-30/banners/banner-2.jpg') }}" alt="Banner">
+                    </figure>
+                    <figure>
+                        <img src="{{ staticAsset('frontend/skinoasis/assets/images/demos/demo-30/banners/banner-3.jpg') }}" alt="Banner">
+                    </figure>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
