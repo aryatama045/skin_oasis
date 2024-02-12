d0200004ab755ca25b82c49f3658c8a3266b5c54
Aryatama <18211338+aryatama045@users.noreply.github.com>
2024-02-12 16:04:51 +0700

    Update 5favoriteproduct.blade.php

--- resources/views/frontend/skinoasis/pages/partials/home/5favoriteproduct.blade.php
+++ resources/views/frontend/skinoasis/pages/partials/home/5favoriteproduct.blade.php
@@ -1,5 +1,5 @@
 <!-- Product Favorite -->
-<section class="pt-8 pb-100 bg-white position-relative overflow-hidden z-1 trending-products-area">
+<div class="seller pt-8 pb-100 bg-white position-relative overflow-hidden z-1 trending-products-area">
     <div class="container">
         <div class="row align-items-center">
             <div class="col-xl-5">
@@ -21,88 +21,8 @@
                 </div>
             </div>
         </div>
-        <div class="row justify-content-center g-4 mt-5 filter_group">
-            <div class="tab-content tab-content-carousel">
-                @php
-                    $trending_products = getSetting('top_trending_products') != null ? json_decode(getSetting('top_trending_products')) : [];
-                    $products = \App\Models\Product::whereIn('id', $trending_products)->get();
-                @endphp
-
-                @foreach ($products as $product)
-
-                <div class="owl-carousel carousel-equal-height owl-simple carousel-with-shadow
-                        row cols-lg-4 cols-md-3 cols-2 filter_item
-                        @php if($product->categories()->count() > 0){
-                            foreach ($product->categories as $category) {
-                                echo $category->id .' ';
-                            }
-                        } @endphp"
-                    data-toggle="owl"
-                    data-owl-options='{
-                        "nav": false,
-                        "dots": true,
-                        "margin": 20,
-                        "loop": false,
-                        "responsive": {
-                            "0": {
-                                "items": 2
-                            },
-                            "768": {
-                                "items": 3
-                            },
-                            "992": {
-                                "items": 4,
-                                "nav": true
-                            }
-                        }
-                    }'>
-                        @include('frontend.default.pages.partials.products.trending-product-card', [
-                            'product' => $product,
-                        ])
-                </div>
-                @endforeach
-        </div>
-    </div>
-</section>
-
-<div class="seller pt-5 pt-md-6 pb-1 pb-lg-3 my-2 mt-0">
-    <div class="container">
-        <div class="heading heading-center mb-5">
-            <h2 class="title text-uppercase mb-3">{{ localize('Products Favorite') }}</h2>
-            <ul class="nav nav-pills filter-btns justify-content-center" role="tablist">
-                @php
-                    $trending_product_categories = getSetting('trending_product_categories') != null ? json_decode(getSetting('trending_product_categories')) : [];
-                    $categories = \App\Models\Category::whereIn('id', $trending_product_categories)->get();
-                @endphp
-                <li class="nav-item">
-                    <a href="#fav-all" class="nav-link font-size-normal letter-spacing-large active" data-toggle="tab" role="tab">{{ localize('All') }}</a>
-                </li>
-                @foreach ($categories as $category)
-                    <li class="nav-item">
-                        <a href="#fav-{{ $category->id }}" class="nav-link font-size-normal letter-spacing-large" data-toggle="tab" role="tab">{{ $category->collectLocalization('name') }}</a>
-                    </li>
-                @endforeach
-            </ul>
-        </div>
-
-        <div class="tab-content tab-content-carousel filter_group">
-            @php
-                $trending_products = getSetting('top_trending_products') != null ? json_decode(getSetting('top_trending_products')) : [];
-                $products = \App\Models\Product::whereIn('id', $trending_products)->get();
-            @endphp
-
-            @foreach ($categories as $category)
-
-            <div class="tab-pane p-0 fade filter-item" role="tabpanel" id="@php
-                        foreach ($products as $key => $product){
-                            if($product->categories()->count() > 0){
-                                foreach ($product->categories as $category) {
-                                    echo 'fav-'.$category->id .' ';
-                                }
-                            }
-                        }
-                        @endphp">
-
+        <div class="row justify-content-center g-4 mt-5 ">
+            <div class="tab-content tab-content-carousel filter_group">
                 <div class="owl-carousel  carousel-equal-height owl-simple carousel-with-shadow row cols-lg-4 cols-md-3 cols-2" data-toggle="owl"
                     data-owl-options='{
                         "nav": false,
@@ -122,18 +42,25 @@
                             }
                         }
                     }'>
-                    @foreach ($products as $key => $product)
-                    <div class="product bg-white shadow-none">
-                        @include('frontend.skinoasis.pages.partials.products.trending-product-card', [
-                            'product' => $product,
-                        ])
-                    </div>
+                    @php
+                        $trending_products = getSetting('top_trending_products') != null ? json_decode(getSetting('top_trending_products')) : [];
+                        $products = \App\Models\Product::whereIn('id', $trending_products)->get();
+                    @endphp
+
+                    @foreach ($products as $product)
+                        <div class="filter_item
+                            @php if($product->categories()->count() > 0){
+                                    foreach ($product->categories as $category) {
+                                        echo $category->id .' ';
+                                    }
+                                } @endphp">
+                            @include('frontend.default.pages.partials.products.trending-product-card', [
+                                'product' => $product,
+                            ])
+                        </div>
                     @endforeach
                 </div>
             </div>
-            @endforeach
-
-
         </div>
     </div>
 </div>
