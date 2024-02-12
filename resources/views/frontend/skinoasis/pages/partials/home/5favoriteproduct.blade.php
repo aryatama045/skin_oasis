e36c8b3891c967b994b849a48d7ac985280e4f6b
Aryatama <18211338+aryatama045@users.noreply.github.com>
2024-02-12 14:52:32 +0700

    home update

--- resources/views/frontend/skinoasis/pages/partials/home/5favoriteproduct.blade.php
+++ resources/views/frontend/skinoasis/pages/partials/home/5favoriteproduct.blade.php
@@ -1,77 +1,46 @@
 <!-- Product Favorite -->
-<div class="pt-6 pb-6" style="background-color: #fff;">
+<section class="pt-8 pb-100 bg-white position-relative overflow-hidden z-1 trending-products-area">
     <div class="container">
-        <div class="banner-set">
-            <div class="row">
+        <div class="row align-items-center">
+            <div class="col-xl-5">
+                <div class="section-title text-center text-xl-start">
+                    <h3 class="mb-0">{{ localize('Top Trending Products') }}</h3>
+                </div>
+            </div>
+            <div class="col-xl-7">
+                <div class="filter-btns gshop-filter-btn-group text-center text-xl-end mt-4 mt-xl-0">
 
-                <div class="col-lg-6">
-                    <div class="banner-set-image banner-border-hover">
-                        <a href="#">
-                            <img src="{{ staticAsset('frontend/skinoasis/assets/images/demos/demo-5/banners/banner-4.jpg') }}" alt="banner">
-                        </a>
-                        <div class="banner-content">
-                            <h3 class="banner-title"><a href="#"><span>Casual basics and<br>trendy key pieces.</span></a></h3><!-- End .banner-title -->
-                            <h4 class="banner-subtitle">in this look</h4>
-                            <!-- End .banner-subtitle -->
-                            <h4 class="banner-detail">• Rib-knit cardigan <br>• Linen-blend paper bag trousers</h4>
-                            <h4 class="banner-price">$19.99 - $48.00</h4>
-                            <a href="#" class="btn btn-outline-primary-2 banner-link">buy all</a>
-                        </div><!-- End .banner-content -->
-                    </div><!-- End .banner-set-image -->
-                </div><!-- End .col-lg-6 -->
+                    @php
+                        $trending_product_categories = getSetting('trending_product_categories') != null ? json_decode(getSetting('trending_product_categories')) : [];
+                        $categories = \App\Models\Category::whereIn('id', $trending_product_categories)->get();
+                    @endphp
+                    <button class="active" data-filter="*">{{ localize('All Products') }}</button>
+                    @foreach ($categories as $category)
+                        <button data-filter=".{{ $category->id }}">{{ $category->collectLocalization('name') }}</button>
+                    @endforeach
+                </div>
+            </div>
+        </div>
+        <div class="row justify-content-center g-4 mt-5 filter_group">
 
-                <div class="col-lg-6">
-                    <div class="banner-set-content text-center">
-                        <div class="set-content-wrapper">
-                            <h4>Special</h4>
-                            <h2>Refine Your Style.</h2>
+            @php
+                $trending_products = getSetting('top_trending_products') != null ? json_decode(getSetting('top_trending_products')) : [];
+                $products = \App\Models\Product::whereIn('id', $trending_products)->get();
+            @endphp
 
-                            <p>Get on our exclusive email list and be the first to hear about sales, coupons, new arrivals and more! </p>
-
-                            <div class="banner-set-products">
-                                <div class="row">
-                                    <div class="products">
-                                        <div class="col-6">
-                                            <div class="product product-2 text-center">
-                                                <figure class="product-media">
-                                                    <a href="#">
-                                                        <img src="{{ staticAsset('frontend/skinoasis/assets/images/demos/demo-5/products/product-13.jpg') }}" alt="Product image" class="product-image">
-                                                    </a>
-                                                </figure><!-- End .product-media -->
-
-                                                <div class="product-body">
-                                                    <h3 class="product-title"><a href="#">Rib-knit cardigan</a></h3><!-- End .product-title -->
-                                                    <div class="product-price">
-                                                        $24.99
-                                                    </div><!-- End .product-price -->
-                                                </div><!-- End .product-body -->
-                                            </div><!-- End .product -->
-                                        </div><!-- End .col-sm-6 -->
-
-                                        <div class="col-6">
-                                            <div class="product product-2 text-center">
-                                                <figure class="product-media">
-                                                    <a href="#">
-                                                        <img src="{{ staticAsset('frontend/skinoasis/assets/images/demos/demo-5/products/product-14.jpg') }}" alt="Product image" class="product-image">
-                                                    </a>
-                                                </figure><!-- End .product-media -->
-
-                                                <div class="product-body">
-                                                    <h3 class="product-title"><a href="#">Linen-blend trousers</a></h3><!-- End .product-title -->
-                                                    <div class="product-price">
-                                                        $19.99
-                                                    </div><!-- End .product-price -->
-                                                </div><!-- End .product-body -->
-                                            </div><!-- End .product -->
-                                        </div><!-- End .col-sm-6 -->
-                                    </div>
-                                </div><!-- End .row -->
-                            </div><!-- End .banner-set-products -->
-                        </div><!-- End .set-content-wrapper -->
-                    </div><!-- End .banner-set-content -->
-                </div><!-- End .col-lg-6 -->
-
-            </div><!-- End .row -->
-        </div><!-- End .banner-set -->
-    </div><!-- End .container -->
-</div><!-- End .bg-lighter pt6 pb-6 -->
+            @foreach ($products as $product)
+                <div
+                    class="col-xxl-3 col-lg-4 col-md-6 col-sm-10 filter_item
+                    @php if($product->categories()->count() > 0){
+                            foreach ($product->categories as $category) {
+                                echo $category->id .' ';
+                            }
+                        } @endphp">
+                    @include('frontend.default.pages.partials.products.trending-product-card', [
+                        'product' => $product,
+                    ])
+                </div>
+            @endforeach
+        </div>
+    </div>
+</section>
