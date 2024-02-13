53cdf0877a1eae779e1e9f6c1ab02ccae2b35769
Aryatama <18211338+aryatama045@users.noreply.github.com>
2024-02-13 13:30:59 +0700

    css

--- resources/views/frontend/skinoasis/pages/partials/home/6bannerstwo.blade.php
+++ resources/views/frontend/skinoasis/pages/partials/home/6bannerstwo.blade.php
@@ -5,8 +5,8 @@
                 <div class="clearfix">
                     <div class="heading mt-3">
                         <h3 class="">Material &amp; Design</h3>
-                        <h2 class="title mb-2">The ultimate dream of SKINOASIS is make people beauty, healthy and happy</h2>
-                            <img class="col-md-6 float-md-end mb-3 ms-md-3"
+                        <h2 class="title-banner-2 mb-2">The ultimate dream of SKINOASIS is make people beauty, healthy and happy</h2>
+                            <img class="col-md-8 float-md-end mb-3 ms-md-3"
                             src="{{ staticAsset('frontend/skinoasis/assets/images/demos/demo-30/banners/banner-2.jpg') }}"  alt="Banner">
                     </div>
                         <p class="content mb-6">
