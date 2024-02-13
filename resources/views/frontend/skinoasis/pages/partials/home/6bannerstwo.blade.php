7ac4eff2097d025665a7487ec66499624c1dfb04
Aryatama <18211338+aryatama045@users.noreply.github.com>
2024-02-13 13:09:33 +0700

    Update 6bannerstwo.blade.php

--- resources/views/frontend/skinoasis/pages/partials/home/6bannerstwo.blade.php
+++ resources/views/frontend/skinoasis/pages/partials/home/6bannerstwo.blade.php
@@ -1,5 +1,5 @@
 <section class="gallery pt-15 pb-15" style="background-image: url({{ staticAsset('frontend/skinoasis/assets/images/demos/demo-30/bg-3.png') }})">
-    <div class="container">
+    <div class="container mb-3">
         <div class="row">
             <div class="col-md-4">
                 <div class="content-left">
