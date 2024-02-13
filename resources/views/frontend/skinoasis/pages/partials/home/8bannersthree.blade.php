62004abb365504431923858b1756d28a08cce352
Aryatama <18211338+aryatama045@users.noreply.github.com>
2024-02-13 15:35:34 +0700

    Update 8bannersthree.blade.php

--- resources/views/frontend/skinoasis/pages/partials/home/8bannersthree.blade.php
+++ resources/views/frontend/skinoasis/pages/partials/home/8bannersthree.blade.php
@@ -1,20 +1,19 @@
 <section class="bg-white position-relative overflow-hidden z-1">
     <div class="row align-items-center">
-        <div class="col-md-6 mb-3 mb-md-0">
-            <div class="video-box-content">
-                <h3 class="video-banner-title h1"><span class="text-primary">New Video</span><strong>Deal Banner</strong></h3><!-- End .video-banner-title -->
-                <p>Lorem ipsum dolor sit amet, consecte adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis.</p>
-                <a href="#" class="btn btn-outline-primary"><span>Click Here</span><i class="icon-long-arrow-right"></i></a>
-            </div><!-- End .video-box-content -->
-        </div><!-- End .col-md-6 -->
         <div class="col-md-6">
             <div class="video-poster">
                 <img src="{{ staticAsset('frontend/skinoasis/assets/images/banners/banner3-1.png') }}" alt="poster">
-
                 <div class="video-poster-content">
                     <a href="https://www.youtube.com/watch?v=vBPgmASQ1A0" class="btn-video btn-iframe"><i class="icon-play"></i></a>
-                </div><!-- End .video-poster-content -->	
+                </div><!-- End .video-poster-content -->
             </div><!-- End .video-poster -->
         </div><!-- End .col-md-6 -->
+
+        <div class="col-md-6 mb-3 mb-md-0">
+                <h3 class="video-banner-title h1"><span class="text-primary">New Video</span><strong>Deal Banner</strong></h3><!-- End .video-banner-title -->
+                <p>Lorem ipsum dolor sit amet, consecte adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis.</p>
+                <a href="#" class="btn btn-outline-primary"><span>Click Here</span><i class="icon-long-arrow-right"></i></a>
+        </div><!-- End .col-md-6 -->
+
     </div>
 </section>
