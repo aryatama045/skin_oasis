ea1b709360f7fd215f68230eac1977c295c6f489
Aryatama <18211338+aryatama045@users.noreply.github.com>
2024-02-13 15:51:54 +0700

    Update 8bannersthree.blade.php

--- resources/views/frontend/skinoasis/pages/partials/home/8bannersthree.blade.php
+++ resources/views/frontend/skinoasis/pages/partials/home/8bannersthree.blade.php
@@ -1,10 +1,10 @@
-<section class="bg-white position-relative overflow-hidden z-1">
+<section class="gallery bg-white position-relative overflow-hidden z-1">
     <div class="video-banner video-banner-poster text-left mt-10">
         <div class="container">
             <div class="row align-items-center">
                 <div class="col-md-6">
-                    <div class="video-poster">
-                        <img src="{{ staticAsset('frontend/skinoasis/assets/images/banners/banner3-1.png') }}" alt="poster">
+                    <div class="video-poster content-right">
+                        <img style="border-radius: 0 !important;" src="{{ staticAsset('frontend/skinoasis/assets/images/banners/banner3-1.png') }}" alt="poster">
                         <div class="video-poster-content">
                             <a href="https://www.youtube.com/watch?v=vBPgmASQ1A0" class="btn-video btn-iframe"><i class="icon-play"></i></a>
                         </div><!-- End .video-poster-content -->
@@ -20,6 +20,34 @@
                         We want you to be able to try everything at once and make sure that our selection of products is about quality.</p>
                     <a href="#" class="btn btn-outline"><span>Click Here</span><i class="icon-long-arrow-right"></i></a>
                 </div><!-- End .col-md-6 -->
+            </div><!-- End .row -->
+        </div><!-- End .container -->
+    </div>
+
+    <div class="video-banner video-banner-poster text-left mt-10">
+        <div class="container">
+            <div class="row align-items-center">
+
+                <div class="col-md-4 mb-3 mb-md-0">
+                    <h3 class="video-banner-title fw-bold h3">
+                        <span class="fw-light">TRENDING</span>
+                        Always Make Room for a Little Beauty in Your Life
+                    </h3><!-- End .video-banner-title -->
+                    <p>Our beauty box is a set of best full-size products that are top sellers in out online shop.
+                        We want you to be able to try everything at once and make sure that our selection of products is about quality.</p>
+                    <a href="#" class="btn btn-outline"><span>Click Here</span><i class="icon-long-arrow-right"></i></a>
+                </div><!-- End .col-md-4 -->
+
+                <div class="col-md-4">
+                    <div class="video-poster">
+                        <img style="border-radius: 0 !important;" src="{{ staticAsset('frontend/skinoasis/assets/images/banners/banner3-1.png') }}" alt="poster">
+                        <div class="video-poster-content">
+                            <a href="https://www.youtube.com/watch?v=vBPgmASQ1A0" class="btn-video btn-iframe"><i class="icon-play"></i></a>
+                        </div><!-- End .video-poster-content -->
+                    </div><!-- End .video-poster -->
+                </div><!-- End .col-md-4 -->
+
+
             </div><!-- End .row -->
         </div><!-- End .container -->
     </div>
