672076f176971dfc54474468ef041df722a3d2b4
Aryatama <18211338+aryatama045@users.noreply.github.com>
2024-02-13 13:13:31 +0700

    Update 6bannerstwo.blade.php

--- resources/views/frontend/skinoasis/pages/partials/home/6bannerstwo.blade.php
+++ resources/views/frontend/skinoasis/pages/partials/home/6bannerstwo.blade.php
@@ -1,29 +1,26 @@
 <section class="gallery pt-15 pb-15" style="background-image: url({{ staticAsset('frontend/skinoasis/assets/images/demos/demo-30/bg-3.png') }})">
     <div class="container mb-3">
         <div class="row">
-            <div class="col-md-4">
+            <div class="col-md-8">
                 <div class="content-left">
-                    <div class="heading mt-3">
-                        <h3 class="">Material &amp; Design</h3>
-                        <h2 class="title mb-2">Product <br>Gallery</h2>
-                        <p class="content mb-6">
-                            Donec nec justo eget felis facilisis fermentum. Aliquam <br>
-                            porttitor mauris sit amet orci. Aenean dignissim <br>
-                            pellentesque felis.
-                        </p>
+                    <div class="clearfix">
+                        <div class="heading mt-3">
+                            <h3 class="">Material &amp; Design</h3>
+                            <h2 class="title mb-2">Product <br>Gallery</h2>
+                            <p class="content mb-6">
+                                Donec nec justo eget felis facilisis fermentum. Aliquam <br>
+                                porttitor mauris sit amet orci. Aenean dignissim <br>
+                                pellentesque felis.
+                            </p>
+                        </div>
+                        <figure>
+                            <img class="col-md-6 float-md-end mb-3 ms-md-3"
+                                src="{{ staticAsset('frontend/skinoasis/assets/images/demos/demo-30/banners/banner-3.jpg') }}"  alt="Banner">
+                        </figure>
                     </div>
-                    <figure>
-                        <img src="{{ staticAsset('frontend/skinoasis/assets/images/demos/demo-30/banners/banner-3.jpg') }}" alt="Banner">
-                    </figure>
                 </div>
             </div>
 
-            <div class="col-md-4">
-                <figure>
-                    <img src="{{ staticAsset('frontend/skinoasis/assets/images/demos/demo-30/banners/banner-1.jpg') }}" alt="Banner">
-                </figure>
-
-            </div>
             <div class="col-md-4 p-md-0">
                 <div class="content-right">
                     <figure class="mb-md-12">
