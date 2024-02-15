b0e7921c99adf0dec76cf155a1f88d64551a8d23
Aryatama <18211338+aryatama045@users.noreply.github.com>
2024-02-15 14:11:41 +0700

    Update pricing.blade.php

--- resources/views/frontend/skinoasis/pages/partials/products/pricing.blade.php
+++ resources/views/frontend/skinoasis/pages/partials/products/pricing.blade.php
@@ -1,15 +1,15 @@
 @if (productBasePrice($product) == discountedProductBasePrice($product))
     @if (productBasePrice($product) == productMaxPrice($product))
-        <h4 class="fw-bold ">{{ formatPrice(productBasePrice($product)) }}</h4>
+        <h4 class="fw-semibold ">{{ formatPrice(productBasePrice($product)) }}</h4>
     @else
-        <h4 class="fw-bold ">{{ formatPrice(productBasePrice($product)) }} - {{ formatPrice(productMaxPrice($product)) }}</h4>
+        <h4 class="fw-semibold ">{{ formatPrice(productBasePrice($product)) }} - {{ formatPrice(productMaxPrice($product)) }}</h4>
 
     @endif
 @else
     @if (discountedProductBasePrice($product) == discountedProductMaxPrice($product))
-        <h4 class="fw-bold ">{{ formatPrice(discountedProductBasePrice($product)) }}</h4>
+        <h4 class="fw-semibold ">{{ formatPrice(discountedProductBasePrice($product)) }}</h4>
     @else
-        <h4 class="fw-bold ">{{ formatPrice(discountedProductBasePrice($product)) }} - {{ formatPrice(discountedProductMaxPrice($product)) }}</h4>
+        <h4 class="fw-semibold ">{{ formatPrice(discountedProductBasePrice($product)) }} - {{ formatPrice(discountedProductMaxPrice($product)) }}</h4>
     @endif
 
     @if (isset($br))
@@ -19,9 +19,9 @@
     @if (!isset($onlyPrice) || $onlyPrice == false)
         @if (productBasePrice($product) == productMaxPrice($product))
             <h4
-                class="fw-bold deleted text-muted {{ isset($br) ? '' : 'ms-1' }}">{{ formatPrice(productBasePrice($product)) }}</h4>
+                class="fw-semibold deleted text-muted {{ isset($br) ? '' : 'ms-1' }}">{{ formatPrice(productBasePrice($product)) }}</h4>
         @else
-            <h4 class="fw-bold deleted text-muted {{ isset($br) ? '' : 'ms-1' }}">{{ formatPrice(productBasePrice($product)) }} - {{ formatPrice(productMaxPrice($product)) }}</h4>
+            <h4 class="fw-semibold deleted text-muted {{ isset($br) ? '' : 'ms-1' }}">{{ formatPrice(productBasePrice($product)) }} - {{ formatPrice(productMaxPrice($product)) }}</h4>
 
         @endif
     @endif
