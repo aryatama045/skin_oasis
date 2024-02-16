767ef8a47df3c1ad8b928c06e4765dc3ff2d77c5
Aryatama <18211338+aryatama045@users.noreply.github.com>
2024-02-16 09:09:33 +0700

    Create Partner.php

---
+++ app/Models/Partner.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App;
+
+class Partner extends Model
+{
+    use HasFactory;
+    protected $table = "partner_pages";
+    protected $primaryKey = 'id';
+    protected $fillable = [
+        'title',
+        'slug',
+        'category',
+        'content',
+        'meta_title',
+        'meta_image',
+        'meta_description'
+    ];
+}
