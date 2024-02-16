2d9601780a5c093a89db2b656e03faed063b74e3
Aryatama <18211338+aryatama045@users.noreply.github.com>
2024-02-16 11:49:56 +0700

    update header

--- resources/views/frontend/skinoasis/inc/header.blade.php
+++ resources/views/frontend/skinoasis/inc/header.blade.php
@@ -15,9 +15,9 @@
                 <a href="{{ route('home') }}" class="logo">
                     @if(!empty(Route::current()->getName()))
                         @if(Route::current()->getName() == 'home' || Route::current()->getName() == 'home.pages.aboutUs')
-                            <img src="{{ staticAsset('frontend/skinoasis/assets/images/logo.png') }}" alt="SKINOASIS Logo" width="200" height="25">
+                            <img src="{{ staticAsset('frontend/skinoasis/assets/images/logo.png') }}" alt="SKINOASIS Logo" width="300" height="25">
                         @else
-                        <img src="{{ staticAsset('frontend/skinoasis/assets/images/logo-black.png') }}" alt="SKINOASIS Logo" width="200" height="25">
+                        <img src="{{ staticAsset('frontend/skinoasis/assets/images/logo-black.png') }}" alt="SKINOASIS Logo" width="300" height="25">
                         @endif
                     @endif
                 </a>
