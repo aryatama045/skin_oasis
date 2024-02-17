f3a6aa6ff7d193982ef352df0703a61e96c7ba0f
Aryatama <18211338+aryatama045@users.noreply.github.com>
2024-02-17 20:24:39 +0700

    Update scripts.blade.php

--- resources/views/frontend/skinoasis/inc/scripts.blade.php
+++ resources/views/frontend/skinoasis/inc/scripts.blade.php
@@ -22,9 +22,12 @@
 
 
 <!-- Main JS File -->
+
+<script src="{{ staticAsset('frontend/skinoasis/assets/main.js') }}"></script>
+<script src="{{ staticAsset('frontend/skinoasis/assets/main.min.js') }}"></script>
+
 <script src="{{ staticAsset('frontend/skinoasis/assets/js/main.js') }}"></script>
 <script src="{{ staticAsset('frontend/skinoasis/assets/js/main.min.js') }}"></script>
-
 
 <script src="{{ staticAsset('frontend/default/assets/js/vendors/counterup.min.js') }}"></script>
 <script src="{{ staticAsset('frontend/default/assets/js/vendors/clipboard.min.js') }}"></script>
