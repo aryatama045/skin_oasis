549733118bf349cb994acb4c4a53a741aaf2fdae
Aryatama <18211338+aryatama045@users.noreply.github.com>
2024-02-19 13:44:49 +0700

    Update styles.blade.php

--- resources/views/backend/inc/styles.blade.php
+++ resources/views/backend/inc/styles.blade.php
@@ -27,6 +27,22 @@
       menubar: 'favs file edit view insert format tools table help',
       content_css: 'css/content.css',
       content_style:"@import url('https://fonts.cdnfonts.com/css/avenir-lt-pro'); body { font-family:'Avenir LT Pro', sans-serif;}",
+      file_picker_callback: function(callback, value, meta) {
+        // Provide file and text for the link dialog
+        if (meta.filetype == 'file') {
+          callback('mypage.html', {text: 'My text'});
+        }
+
+        // Provide image and alt text for the image dialog
+        if (meta.filetype == 'image') {
+          callback('myimage.jpg', {alt: 'My alt text'});
+        }
+
+        // Provide alternative source and posted for the media dialog
+        if (meta.filetype == 'media') {
+          callback('movie.mp4', {source2: 'alt.ogg', poster: 'image.jpg'});
+        }
+      }
     });
   </script>
 <style>
