295b8f030b764f8e4aaca377bd2d3b5b7b71d375
Aryatama <18211338+aryatama045@users.noreply.github.com>
2024-02-19 16:20:49 +0700

    Update bottomAbout.blade.php

--- resources/views/frontend/skinoasis/pages/quickLinks/bottomAbout.blade.php
+++ resources/views/frontend/skinoasis/pages/quickLinks/bottomAbout.blade.php
@@ -13,15 +13,55 @@
                 @endif
 
                 <span class="title-separator">|</span>
-                PION TECH
+                @if (url()->current() == url('pages/pion-tech'))
+                    <span class="fw-bolder">
+                        <a class="text-dark" href="{{url('pages/pion-tech')}}">PION TECH</a>
+                    </span>
+                @else
+                    <span class="fw-normal">
+                        <a class="text-dark" href="{{url('pages/pion-tech')}}">PION TECH</a>
+                    </span>
+                @endif
                 <span class="title-separator">|</span>
-                VEGETOLOGY
+                @if (url()->current() == url('pages/vegetology'))
+                    <span class="fw-bolder">
+                        <a class="text-dark" href="{{url('pages/vegetology')}}">VEGETOLOGY</a>
+                    </span>
+                @else
+                    <span class="fw-normal">
+                        <a class="text-dark" href="{{url('pages/vegetology')}}">VEGETOLOGY</a>
+                    </span>
+                @endif
                 <span class="title-separator">|</span>
-                SHINSIAVIEW
+                @if (url()->current() == url('pages/shinsiaview'))
+                    <span class="fw-bolder">
+                        <a class="text-dark" href="{{url('pages/shinsiaview')}}">SHINSIAVIEW</a>
+                    </span>
+                @else
+                    <span class="fw-normal">
+                        <a class="text-dark" href="{{url('pages/shinsiaview')}}">SHINSIAVIEW</a>
+                    </span>
+                @endif
                 <span class="title-separator">|</span>
-                LEAF & COCO
+                @if (url()->current() == url('pages/leaf-coco'))
+                    <span class="fw-bolder">
+                        <a class="text-dark" href="{{url('pages/leaf-coco')}}">LEAF & COCO</a>
+                    </span>
+                @else
+                    <span class="fw-normal">
+                        <a class="text-dark" href="{{url('pages/leaf-coco')}}">LEAF & COCO</a>
+                    </span>
+                @endif
                 <span class="title-separator">|</span>
-                BAIN & CO
+                @if (url()->current() == url('pages/bain-co'))
+                    <span class="fw-bolder">
+                        <a class="text-dark" href="{{url('pages/bain-co')}}">BAIN & CO</a>
+                    </span>
+                @else
+                    <span class="fw-normal">
+                        <a class="text-dark" href="{{url('pages/bain-co')}}">BAIN & CO</a>
+                    </span>
+                @endif
             </div>
         </div>
     </div>
