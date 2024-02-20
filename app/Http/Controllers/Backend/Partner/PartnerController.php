7b5b0831bf4b1b31c001b88d1d8ba155e4efa355
Aryatama <18211338+aryatama045@users.noreply.github.com>
2024-02-20 13:59:23 +0700

    Update PartnerController.php

--- app/Http/Controllers/Backend/Partner/PartnerController.php
+++ app/Http/Controllers/Backend/Partner/PartnerController.php
@@ -23,7 +23,7 @@ class PartnerController extends Controller
         $searchKey = null;
         $is_published = null;
 
-        $pages = Partner::orderBy('id', 'DESC')->get();
+        $pages = Partner::all();
         if ($request->search != null) {
             $pages = $pages->where('title', 'like', '%' . $request->search . '%');
             $searchKey = $request->search;
