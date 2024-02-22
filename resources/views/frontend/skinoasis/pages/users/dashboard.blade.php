b0f2cdb9cf8a4404b06964d40dba83be47d9a655
Aryatama <18211338+aryatama045@users.noreply.github.com>
2024-02-22 16:36:55 +0700

    Update dashboard.blade.php

--- resources/views/frontend/skinoasis/pages/users/dashboard.blade.php
+++ resources/views/frontend/skinoasis/pages/users/dashboard.blade.php
@@ -136,13 +136,13 @@
                     <img src="{{ staticAsset('frontend/skinoasis/assets/images/icons/icon-send.JPG') }}" alt="icon" class="icon-img">
                     <div class="icon-content">
                         <h5 class="mb-3">INVITE YOUR FRIENDS</h5>
-                        <p class="text-dark mb-0">
+                        <p class="text-dark mb-2">
                             Bagikan link referral ke temanmu via media sosial, kontak, atau email.
                         </p>
-                        <ul class="list-inline small text-muted mt-3 mb-0">
+                        <!-- <ul class="list-inline small text-muted mt-3 mb-0">
                             <li class="list-inline-item"><i class="fa fa-comment-o mr-2"></i>12 Comments</li>
                             <li class="list-inline-item"><i class="fa fa-heart-o mr-2"></i>200 Likes</li>
-                        </ul>
+                        </ul> -->
                     </div>
                     <div class="clearfix"></div>
                 </div>
@@ -151,13 +151,13 @@
                     <img src="{{ staticAsset('frontend/skinoasis/assets/images/icons/icon-form.JPG') }}" alt="icon" class="icon-img">
                     <div class="icon-content">
                         <h5 class="mb-3">REGISTRATION & VERIFICATION</h5>
-                        <p class="text-dark mb-0">
+                        <p class="text-dark mb-2">
                             Setelah berhasil mendaftarkan diri melalui link referral, memverifikasi nomor HP, dan melengkapi Profile, temanmu akan mendapatkan voucher 20% untuk belanja cantik pertamanya di SKINOASIS
                         </p>
-                        <ul class="list-inline small text-muted mt-3 mb-0">
+                        <!-- <ul class="list-inline small text-muted mt-3 mb-0">
                             <li class="list-inline-item"><i class="fa fa-comment-o mr-2"></i>12 Comments</li>
                             <li class="list-inline-item"><i class="fa fa-heart-o mr-2"></i>200 Likes</li>
-                        </ul>
+                        </ul> -->
                     </div>
                 </div>
             </div>
