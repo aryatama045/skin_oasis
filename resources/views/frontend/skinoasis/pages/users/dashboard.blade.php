12192435059348cb35849ed9ef49e91b1e128c1b
Aryatama <18211338+aryatama045@users.noreply.github.com>
2024-02-22 13:41:34 +0700

    Update dashboard.blade.php

--- resources/views/frontend/skinoasis/pages/users/dashboard.blade.php
+++ resources/views/frontend/skinoasis/pages/users/dashboard.blade.php
@@ -78,9 +78,9 @@
                 </ul>
             </div>
 
-            <div class="py-4 px-4">
+            <div class="py-4 px-4 mt-5">
 
-                <div class="row mt-5">
+                <div class="row">
                     <div class="col-xl-9">
                         <div class="recent-orders shadow bg-white rounded py-5">
 
@@ -88,22 +88,19 @@
                     </div>
 
                     <div class="col-xl-3">
-                        <div class="recent-orders shadow bg-white rounded py-5">
-
+                        <div class="bg-light rounded shadow bg-white rounded py-5">
+                            <h5 class="mb-3">Recent posts</h5>
+                            <div class="p-4 ">
+                                <p class="font-italic mb-0">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+                                <ul class="list-inline small text-muted mt-3 mb-0">
+                                    <li class="list-inline-item"><i class="fa fa-comment-o mr-2"></i>12 Comments</li>
+                                    <li class="list-inline-item"><i class="fa fa-heart-o mr-2"></i>200 Likes</li>
+                                </ul>
+                            </div>
                         </div>
                     </div>
                 </div>
 
-                <div class="py-4">
-                    <h5 class="mb-3">Recent posts</h5>
-                    <div class="p-4 bg-light rounded shadow-sm">
-                        <p class="font-italic mb-0">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
-                        <ul class="list-inline small text-muted mt-3 mb-0">
-                            <li class="list-inline-item"><i class="fa fa-comment-o mr-2"></i>12 Comments</li>
-                            <li class="list-inline-item"><i class="fa fa-heart-o mr-2"></i>200 Likes</li>
-                        </ul>
-                    </div>
-                </div>
             </div>
         </div><!-- End profile widget -->
 
