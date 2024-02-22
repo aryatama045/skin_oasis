82552c6950bc879a5a2caf167e69506ae710b605
Aryatama <18211338+aryatama045@users.noreply.github.com>
2024-02-22 16:02:48 +0700

    Update dashboard.blade.php

--- resources/views/frontend/skinoasis/pages/users/dashboard.blade.php
+++ resources/views/frontend/skinoasis/pages/users/dashboard.blade.php
@@ -51,6 +51,10 @@
         border-bottom-color: rgba(93, 80, 5, 1);
         font-weight: 600;
     }
+
+    .img-icon {
+        float: left;
+    }
 </style>
 
 
@@ -99,7 +103,9 @@
                 <div class="rounded shadow-box bg-white ">
                     <div class="p-4 ">
                         <h5 class="mb-3">INVITE YOUR FRIENDS</h5>
-                        <p class="font-italic mb-0">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+                        <p class="font-italic mb-0">
+                            <img src="https://www.w3schools.com/css/pineapple.jpg" alt="Pineapple" class="img-icon">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
                         <ul class="list-inline small text-muted mt-3 mb-0">
                             <li class="list-inline-item"><i class="fa fa-comment-o mr-2"></i>12 Comments</li>
                             <li class="list-inline-item"><i class="fa fa-heart-o mr-2"></i>200 Likes</li>
@@ -110,7 +116,9 @@
                 <div class="rounded shadow-box bg-white mt-5">
                     <div class="p-4 ">
                         <h5 class="mb-3">REGISTRATION & VERIFICATION</h5>
-                        <p class="font-italic mb-0">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+                        <p class="font-italic mb-0">
+                            <img src="https://www.w3schools.com/css/pineapple.jpg" alt="Pineapple" class="img-icon">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
                         <ul class="list-inline small text-muted mt-3 mb-0">
                             <li class="list-inline-item"><i class="fa fa-comment-o mr-2"></i>12 Comments</li>
                             <li class="list-inline-item"><i class="fa fa-heart-o mr-2"></i>200 Likes</li>
