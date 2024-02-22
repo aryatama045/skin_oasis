da63311f63acdb678e01bcec0699543bcabc3b0e
Aryatama <18211338+aryatama045@users.noreply.github.com>
2024-02-22 12:02:14 +0700

    Update 4bestdeals.blade.php

--- resources/views/frontend/skinoasis/pages/partials/home/4bestdeals.blade.php
+++ resources/views/frontend/skinoasis/pages/partials/home/4bestdeals.blade.php
@@ -29,7 +29,7 @@
                             <!-- <span class="old-price">Was $240.00</span> -->
                         </div><!-- End .product-price -->
 
-                        <a href="{{ route('home.campaigns.show', $campaign->slug) }}"
+                        <a href="{{ route('home.campaigns.show', $campaigns->slug) }}"
                             class="btn btn-rounded btn-sm text-uppercase btn-outline-green-dark mt-5">{{ localize('Shop Now') }}</a>
 
                     </div><!-- End .deal-content -->
