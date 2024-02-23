5459db2589f8cbcccb79cecc08d70b16910b5279
Aryatama <18211338+aryatama045@users.noreply.github.com>
2024-02-23 17:17:18 +0700

    Update scripts.blade.php

--- resources/views/frontend/skinoasis/inc/scripts.blade.php
+++ resources/views/frontend/skinoasis/inc/scripts.blade.php
@@ -1,11 +1,17 @@
 <!-- Plugins JS File -->
+<script src="{{ staticAsset('frontend/default/assets/js/vendors/jquery-3.6.4.min.js') }}"></script>
+
 <script src="{{ staticAsset('frontend/skinoasis/assets/js/jquery.min.js') }}"></script>
 <script src="{{ staticAsset('frontend/default/assets/js/vendors/jquery-ui.min.js') }}"></script>
 <script src="{{ staticAsset('frontend/skinoasis/assets/js/bootstrap.bundle.min.js') }}"></script>
 <script src="{{ staticAsset('frontend/skinoasis/assets/js/jquery.hoverIntent.min.js') }}"></script>
+
+<script src="{{ staticAsset('frontend/skinoasis/assets/js/jquery.waypoints.min.js') }}"></script>
 <script src="{{ staticAsset('frontend/skinoasis/assets/js/superfish.min.js') }}"></script>
 <script src="{{ staticAsset('frontend/skinoasis/assets/js/owl.carousel.min.js') }}"></script>
-<script src="{{ staticAsset('frontend/skinoasis/assets/js/jquery.waypoints.min.js') }}"></script>
+<script src="{{ staticAsset('frontend/skinoasis/assets/js/imagesloaded.pkgd.min.js') }}"></script>
+<script src="{{ staticAsset('frontend/skinoasis/assets/js/isotope.pkgd.min.js') }}"></script>
+
 <script src="{{ staticAsset('frontend/skinoasis/assets/js/wNumb.js') }}"></script>
 <script src="{{ staticAsset('frontend/skinoasis/assets/js/nouislider.min.js') }}"></script>
 <script src="{{ staticAsset('frontend/skinoasis/assets/js/bootstrap-input-spinner.js') }}"></script>
@@ -638,3 +644,4 @@
         @endif
     }
 </script>
+
