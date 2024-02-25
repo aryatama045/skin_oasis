5d512e132beb16bba464d2ae555fdbe35cd2ead5
Aryatama <18211338+aryatama045@users.noreply.github.com>
2024-02-25 10:59:27 +0700

    Update 1hero.blade.php

--- resources/views/frontend/skinoasis/pages/partials/home/1hero.blade.php
+++ resources/views/frontend/skinoasis/pages/partials/home/1hero.blade.php
@@ -4,9 +4,9 @@
 
     @foreach ($sliders as $slider)
         @if ($slider->display_on == 1)
-        <div class="intro-slide animate__animated animate__fadeInUp animate__delay-1s" style="background-image: url({{ uploadedAsset($slider->image) }});">
+        <div class="intro-slide " style="background-image: url({{ uploadedAsset($slider->image) }});">
             <div class="container">
-                <div class="intro-content text-center">
+                <div class="intro-content text-center animate__animated animate__fadeInUp animate__delay-1s">
                     <!-- <h3 class="intro-subtitle cross-txt text-primary">{{ $slider->sub_title }}</h3> -->
                     <h1 class="intro-title text-white">{{ $slider->title }}</h1><!-- End .intro-title -->
                     <div class="intro-text text-white">{{ $slider->text }}</div><!-- End .intro-text -->
