8b9653376d6246423b2c78d0467cab8813e5d3c4
Aryatama <18211338+aryatama045@users.noreply.github.com>
2024-03-04 10:16:54 +0700

    Update 3welcome.blade.php

--- resources/views/frontend/skinoasis/pages/halloBeauty/inc/3welcome.blade.php
+++ resources/views/frontend/skinoasis/pages/halloBeauty/inc/3welcome.blade.php
@@ -1,24 +1,93 @@
 <!-- IG Feed -->
-<div class="bg-lighter pt-5 pb-5" data-aos="fade-up">
+<div class="hallo-beauty bg-lighter pt-5 pb-5" data-aos="fade-up">
     <div class="container">
-        <div class="heading-ig text-left">
-            <h4 class="ls-1px ff">Selamat Datang di</h4><!-- End .title -->
-            <h2 class="text-capitalize ls-1px ff"> Hallo Beauty</h2>
-        </div><!-- End .heading -->
+        <div class="hallo-beauty-heading text-left">
+            <h4 class="heading-subtitle ls-1px ff">Selamat Datang di</h4>
+            <h2 class="heading-title text-capitalize ls-1px ff"> Hallo Beauty</h2>
+        </div>
 
         <div class="row">
-
             <div class="col-sm-6 col-7">
-                <div class="instagram-feed">
-                    <img src="{{ staticAsset('frontend/skinoasis/assets/images/igfeed/1.jpg') }}">
-                    <div class="instagram-feed-content">
-                        <a href="#"><i class="icon-heart-o"></i>280</a>
-                        <a href="#"><i class="icon-comments"></i>22</a>
-                    </div>
+                <div class="rounded shadow-box bg-white py-5">
+                    <h3 class="text-green text-capitalize">Temukan</h3>
                 </div>
+
+                <form action="#">
+                    <div class="row">
+                        <div class="col-6 col-lg-4 col-xl-2">
+                            <div class="btn-wrap">
+                                <a href="#" class="btn btn-outline-primary btn-rounded">
+                                    <span>Semua</span>
+                                    <i class="icon-long-arrow-right"></i>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="col-6 col-lg-4 col-xl-2">
+                            <div class="btn-wrap">
+                                <a href="#" class="btn btn-outline-primary btn-rounded">
+                                    <span>Dokter</span>
+                                    <i class="icon-long-arrow-right"></i>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="col-6 col-lg-4 col-xl-2">
+                            <div class="btn-wrap">
+                                <a href="#" class="btn btn-outline-primary btn-rounded">
+                                    <span>Lokasi</span>
+                                    <i class="icon-long-arrow-right"></i>
+                                </a>
+                            </div>
+                        </div>
+
+                    </div>
+
+                    <div class="input-group">
+                        <input type="text" class="form-control" placeholder="KETIK KATA KUNCI" aria-label="Ketik Kata Kunci" required="">
+                    </div>
+                </form>
+            </div>
+        </div>
+
+        <div class="row elements">
+            <div class="col-xl-5col col-lg-4 col-md-3 col-6">
+                <a href="elements-accordions.html" class="element-type">
+                    <div class="element">
+                        <i class="element-img"></i>
+                        <i class="element-hover-img"></i>
+                        <p>Cari Dokter</p>
+                    </div>
+                </a>
             </div>
 
+            <div class="col-xl-5col col-lg-4 col-md-3 col-6">
+                <a href="elements-accordions.html" class="element-type">
+                    <div class="element">
+                        <i class="element-img"></i>
+                        <i class="element-hover-img"></i>
+                        <p>List Dokter</p>
+                    </div>
+                </a>
+            </div>
 
+            <div class="col-xl-5col col-lg-4 col-md-3 col-6">
+                <a href="elements-accordions.html" class="element-type">
+                    <div class="element">
+                        <i class="element-img"></i>
+                        <i class="element-hover-img"></i>
+                        <p>List Klinik</p>
+                    </div>
+                </a>
+            </div>
+
+            <div class="col-xl-5col col-lg-4 col-md-3 col-6">
+                <a href="elements-accordions.html" class="element-type">
+                    <div class="element">
+                        <i class="element-img"></i>
+                        <i class="element-hover-img"></i>
+                        <p>PAKET KECANTIKAN & PERAWATAN</p>
+                    </div>
+                </a>
+            </div>
         </div>
 
         <div class="more-container text-center">
