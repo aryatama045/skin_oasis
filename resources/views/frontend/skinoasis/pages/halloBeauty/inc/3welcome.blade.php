e4cc0821ff630ae7239a61329353d3618eca551e
Aryatama <18211338+aryatama045@users.noreply.github.com>
2024-03-04 16:21:59 +0700

    Update 3welcome.blade.php

--- resources/views/frontend/skinoasis/pages/halloBeauty/inc/3welcome.blade.php
+++ resources/views/frontend/skinoasis/pages/halloBeauty/inc/3welcome.blade.php
@@ -7,91 +7,91 @@
             </div>
 
             <form action="#">
-            <div class="row">
-                <div class="col-lg-10 ">
-                    <div class="shadow-content rounded">
 
-                    <div class="py-5 p-4">
-                        <h3 class="text-uppercase ls-2px ff">Temukan</h3>
+                <div class="row">
+                    <div class="col-lg-10 ">
+                        <div class="shadow-content rounded">
+
+                            <h3 class="text-uppercase ls-2px ff">Temukan</h3>
+
+                            <!-- Button Group -->
+                            <div class="button-group">
+                                <div class="btn-wrap ">
+                                    <span></span>
+                                    <a href="#" class="btn btn-outline-yellow btn-rounded btn-sm ls-1px text-uppercase">
+                                        <span>Semua</span>
+                                    </a>
+                                </div>
+
+                                <div class="btn-wrap ">
+                                    <span></span>
+                                    <a href="#" class="btn btn-outline-yellow btn-rounded btn-sm ls-1px text-uppercase">
+                                        <span>Dokter</span>
+                                    </a>
+                                </div>
+
+                                <div class="btn-wrap ">
+                                    <span></span>
+                                    <a href="#" class="btn btn-outline-yellow btn-rounded btn-sm ls-1px text-uppercase">
+                                        <span>Lokasi</span>
+                                    </a>
+                                </div>
+                            </div>
+
+                            <!-- Search Key -->
+                            <div class="input-group">
+                                <input type="text" class="form-control search-key" placeholder="KETIK KATA KUNCI" aria-label="Ketik Kata Kunci" >
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row elements">
+                    <div class="col-xl-5col col-lg-3 col-md-3 col-6">
+                        <a href="elements-accordions.html" class="element-type">
+                            <div class="element">
+                                <i class="element-img"></i>
+                                <i class="element-hover-img"></i>
+                                <p>Cari Dokter</p>
+                            </div>
+                        </a>
                     </div>
 
-                    <!-- Button Group -->
-                    <div class="button-group">
-                        <div class="btn-wrap p-4">
-                            <span></span>
-                            <a href="#" class="btn btn-outline-yellow btn-rounded btn-sm ls-1px text-uppercase">
-                                <span>Semua</span>
-                            </a>
-                        </div>
-
-                        <div class="btn-wrap p-4">
-                            <span></span>
-                            <a href="#" class="btn btn-outline-yellow btn-rounded btn-sm ls-1px text-uppercase">
-                                <span>Dokter</span>
-                            </a>
-                        </div>
-
-                        <div class="btn-wrap p-4">
-                            <span></span>
-                            <a href="#" class="btn btn-outline-yellow btn-rounded btn-sm ls-1px text-uppercase">
-                                <span>Lokasi</span>
-                            </a>
-                        </div>
+                    <div class="col-xl-5col col-lg-3 col-md-3 col-6">
+                        <a href="elements-accordions.html" class="element-type">
+                            <div class="element">
+                                <i class="element-img"></i>
+                                <i class="element-hover-img"></i>
+                                <p>List Dokter</p>
+                            </div>
+                        </a>
                     </div>
 
-                    <!-- Search Key -->
-                    <div class="input-group p-4">
-                        <input type="text" class="form-control search-key" placeholder="KETIK KATA KUNCI" aria-label="Ketik Kata Kunci" >
+                    <div class="col-xl-5col col-lg-3 col-md-3 col-6">
+                        <a href="elements-accordions.html" class="element-type">
+                            <div class="element">
+                                <i class="element-img"></i>
+                                <i class="element-hover-img"></i>
+                                <p>List Klinik</p>
+                            </div>
+                        </a>
                     </div>
 
+                    <div class="col-xl-5col col-lg-3 col-md-3 col-6">
+                        <a href="elements-accordions.html" class="element-type">
+                            <div class="element">
+                                <i class="element-img"></i>
+                                <i class="element-hover-img"></i>
+                                <p>PAKET KECANTIKAN &amp; PERAWATAN</p>
+                            </div>
+                        </a>
                     </div>
                 </div>
-            </div>
 
-            <div class="row elements">
-                <div class="col-xl-5col col-lg-3 col-md-3 col-6">
-                    <a href="elements-accordions.html" class="element-type">
-                        <div class="element">
-                            <i class="element-img"></i>
-                            <i class="element-hover-img"></i>
-                            <p>Cari Dokter</p>
-                        </div>
-                    </a>
-                </div>
-
-                <div class="col-xl-5col col-lg-3 col-md-3 col-6">
-                    <a href="elements-accordions.html" class="element-type">
-                        <div class="element">
-                            <i class="element-img"></i>
-                            <i class="element-hover-img"></i>
-                            <p>List Dokter</p>
-                        </div>
-                    </a>
-                </div>
-
-                <div class="col-xl-5col col-lg-3 col-md-3 col-6">
-                    <a href="elements-accordions.html" class="element-type">
-                        <div class="element">
-                            <i class="element-img"></i>
-                            <i class="element-hover-img"></i>
-                            <p>List Klinik</p>
-                        </div>
-                    </a>
-                </div>
-
-                <div class="col-xl-5col col-lg-3 col-md-3 col-6">
-                    <a href="elements-accordions.html" class="element-type">
-                        <div class="element">
-                            <i class="element-img"></i>
-                            <i class="element-hover-img"></i>
-                            <p>PAKET KECANTIKAN &amp; PERAWATAN</p>
-                        </div>
-                    </a>
-                </div>
-            </div>
             </form>
 
-            </div>
+
         </div>
     </div>
 </div>
