d93528089a9b52be92b93108f2917a8bf32e4c88
Aryatama <18211338+aryatama045@users.noreply.github.com>
2024-03-04 16:18:45 +0700

    css

--- resources/views/frontend/skinoasis/pages/halloBeauty/inc/3welcome.blade.php
+++ resources/views/frontend/skinoasis/pages/halloBeauty/inc/3welcome.blade.php
@@ -1,93 +1,94 @@
 <div class="hallo-beauty rounded bg-white bg-white pt-5 pb-5 " data-aos="fade-up">
-    <div class="row p-8">
-        <div class="col-10">
-            <div class="card-body">
-                <div class="heading text-left">
-                    <h4 class="heading-subtitle ls-1px ff">Selamat Datang di</h4>
-                    <h2 class="heading-title text-capitalize ls-1px ff"> Hallo Beauty</h2>
-                </div>
+    <div class="row">
+        <div class="col-12">
+            <div class="heading text-left">
+                <h4 class="heading-subtitle ls-1px ff">Selamat Datang di</h4>
+                <h2 class="heading-title text-capitalize ls-1px ff"> Hallo Beauty</h2>
+            </div>
 
-                <form action="#">
-                <div class="row shadow-content rounded">
-                    <div class="col-lg-10 ">
-                        <div class="py-5 p-4">
-                            <h3 class="text-uppercase ls-2px ff">Temukan</h3>
+            <form action="#">
+            <div class="row">
+                <div class="shadow-content rounded">
+                <div class="col-lg-10 ">
+                    <div class="py-5 p-4">
+                        <h3 class="text-uppercase ls-2px ff">Temukan</h3>
+                    </div>
+
+                    <!-- Button Group -->
+                    <div class="button-group">
+                        <div class="btn-wrap p-4">
+                            <span></span>
+                            <a href="#" class="btn btn-outline-yellow btn-rounded btn-sm ls-1px text-uppercase">
+                                <span>Semua</span>
+                            </a>
                         </div>
 
-                        <!-- Button Group -->
-                        <div class="button-group">
-                            <div class="btn-wrap p-4">
-                                <span></span>
-                                <a href="#" class="btn btn-outline-yellow btn-rounded btn-sm ls-1px text-uppercase">
-                                    <span>Semua</span>
-                                </a>
-                            </div>
-
-                            <div class="btn-wrap p-4">
-                                <span></span>
-                                <a href="#" class="btn btn-outline-yellow btn-rounded btn-sm ls-1px text-uppercase">
-                                    <span>Dokter</span>
-                                </a>
-                            </div>
-
-                            <div class="btn-wrap p-4">
-                                <span></span>
-                                <a href="#" class="btn btn-outline-yellow btn-rounded btn-sm ls-1px text-uppercase">
-                                    <span>Lokasi</span>
-                                </a>
-                            </div>
+                        <div class="btn-wrap p-4">
+                            <span></span>
+                            <a href="#" class="btn btn-outline-yellow btn-rounded btn-sm ls-1px text-uppercase">
+                                <span>Dokter</span>
+                            </a>
                         </div>
 
-                        <!-- Search Key -->
-                        <div class="input-group p-4">
-                            <input type="text" class="form-control search-key" placeholder="KETIK KATA KUNCI" aria-label="Ketik Kata Kunci" >
+                        <div class="btn-wrap p-4">
+                            <span></span>
+                            <a href="#" class="btn btn-outline-yellow btn-rounded btn-sm ls-1px text-uppercase">
+                                <span>Lokasi</span>
+                            </a>
                         </div>
-
                     </div>
+
+                    <!-- Search Key -->
+                    <div class="input-group p-4">
+                        <input type="text" class="form-control search-key" placeholder="KETIK KATA KUNCI" aria-label="Ketik Kata Kunci" >
+                    </div>
+
+                </div>
+                </div>
+            </div>
+
+            <div class="row elements">
+                <div class="col-xl-5col col-lg-3 col-md-3 col-6">
+                    <a href="elements-accordions.html" class="element-type">
+                        <div class="element">
+                            <i class="element-img"></i>
+                            <i class="element-hover-img"></i>
+                            <p>Cari Dokter</p>
+                        </div>
+                    </a>
                 </div>
 
-                <div class="row elements">
-                    <div class="col-xl-5col col-lg-3 col-md-3 col-6">
-                        <a href="elements-accordions.html" class="element-type">
-                            <div class="element">
-                                <i class="element-img"></i>
-                                <i class="element-hover-img"></i>
-                                <p>Cari Dokter</p>
-                            </div>
-                        </a>
-                    </div>
-
-                    <div class="col-xl-5col col-lg-3 col-md-3 col-6">
-                        <a href="elements-accordions.html" class="element-type">
-                            <div class="element">
-                                <i class="element-img"></i>
-                                <i class="element-hover-img"></i>
-                                <p>List Dokter</p>
-                            </div>
-                        </a>
-                    </div>
-
-                    <div class="col-xl-5col col-lg-3 col-md-3 col-6">
-                        <a href="elements-accordions.html" class="element-type">
-                            <div class="element">
-                                <i class="element-img"></i>
-                                <i class="element-hover-img"></i>
-                                <p>List Klinik</p>
-                            </div>
-                        </a>
-                    </div>
-
-                    <div class="col-xl-5col col-lg-3 col-md-3 col-6">
-                        <a href="elements-accordions.html" class="element-type">
-                            <div class="element">
-                                <i class="element-img"></i>
-                                <i class="element-hover-img"></i>
-                                <p>PAKET KECANTIKAN &amp; PERAWATAN</p>
-                            </div>
-                        </a>
-                    </div>
+                <div class="col-xl-5col col-lg-3 col-md-3 col-6">
+                    <a href="elements-accordions.html" class="element-type">
+                        <div class="element">
+                            <i class="element-img"></i>
+                            <i class="element-hover-img"></i>
+                            <p>List Dokter</p>
+                        </div>
+                    </a>
                 </div>
-                </form>
+
+                <div class="col-xl-5col col-lg-3 col-md-3 col-6">
+                    <a href="elements-accordions.html" class="element-type">
+                        <div class="element">
+                            <i class="element-img"></i>
+                            <i class="element-hover-img"></i>
+                            <p>List Klinik</p>
+                        </div>
+                    </a>
+                </div>
+
+                <div class="col-xl-5col col-lg-3 col-md-3 col-6">
+                    <a href="elements-accordions.html" class="element-type">
+                        <div class="element">
+                            <i class="element-img"></i>
+                            <i class="element-hover-img"></i>
+                            <p>PAKET KECANTIKAN &amp; PERAWATAN</p>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            </form>
 
             </div>
         </div>
