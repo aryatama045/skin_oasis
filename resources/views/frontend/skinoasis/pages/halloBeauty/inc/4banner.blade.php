34a9abaee2413a0021e7c6f54cd6330fa7ed7b05
Aryatama <18211338+aryatama045@users.noreply.github.com>
2024-03-04 09:14:00 +0700

    Update 4banner.blade.php

--- resources/views/frontend/skinoasis/pages/halloBeauty/inc/4banner.blade.php
+++ resources/views/frontend/skinoasis/pages/halloBeauty/inc/4banner.blade.php
@@ -1,8 +1,9 @@
 <div class="banner-big" style="background-image: url({{ staticAsset('frontend/skinoasis/assets/images/h-b-banner.png') }}); background-color: #f1f5f7;">
     <div class="banner-content">
-        <div class="banner-subtitle text-secondary">Find online Yoga and Meditation here.</div>
-        <div class="banner-title text-secondary mb-3">Everything For Your Yoga Practice</div>
-        <p class="font-weight-normal mt-3">From mats, props, accessories, and everything in between, delivered direct to your door.</p>
+        <div class="banner-subtitle text-secondary">Cerita</div>
+        <div class="banner-title text-secondary mb-3 ls-1px">#Halo Beauty</div>
+        <p class="font-weight-normal mt-3 ls-1px">Perjuangan Hendra Melawan Jerawat Menahun.</p>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. </p>
         <a href="#" class="btn btn-rounded btn-sm text-uppercase btn-outline-green-dark ls-1">SHOP YOGA MATS</a>
     </div><!--End .banner-content-->
 </div><!--End .banner-big-->
