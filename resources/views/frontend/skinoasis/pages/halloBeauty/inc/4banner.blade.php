47c2c8779684d161cb346b8045860284ad656494
Aryatama <18211338+aryatama045@users.noreply.github.com>
2024-03-04 09:15:08 +0700

    Update 4banner.blade.php

--- resources/views/frontend/skinoasis/pages/halloBeauty/inc/4banner.blade.php
+++ resources/views/frontend/skinoasis/pages/halloBeauty/inc/4banner.blade.php
@@ -4,6 +4,6 @@
         <div class="banner-title text-secondary mb-3 ls-1px">#Halo Beauty</div>
         <p class="font-weight-normal mt-3 ls-1px">Perjuangan Hendra Melawan Jerawat Menahun.</p>
         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. </p>
-        <a href="#" class="btn btn-rounded btn-sm text-uppercase btn-outline-green-dark ls-1">SHOP YOGA MATS</a>
+        <a href="#" class="btn btn-rounded btn-sm text-uppercase btn-outline-green-dark ls-1">Lihat Selengkapnya</a>
     </div><!--End .banner-content-->
 </div><!--End .banner-big-->
