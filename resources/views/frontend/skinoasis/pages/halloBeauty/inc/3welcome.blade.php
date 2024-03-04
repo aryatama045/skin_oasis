033188644e0afb6a4467f39abc1d33ab59eba2ce
Aryatama <18211338+aryatama045@users.noreply.github.com>
2024-03-04 14:16:34 +0700

    Update 3welcome.blade.php

--- resources/views/frontend/skinoasis/pages/halloBeauty/inc/3welcome.blade.php
+++ resources/views/frontend/skinoasis/pages/halloBeauty/inc/3welcome.blade.php
@@ -1,4 +1,4 @@
-<div class="hallo-beauty rounded  bg-white  bg-white p-14 pt-5 pb-5 " data-aos="fade-up">
+<div class="hallo-beauty rounded bg-white bg-white pt-5 pb-5 " data-aos="fade-up">
     <div class="row p-8">
         <div class="col-10">
             <div class="card-body">
