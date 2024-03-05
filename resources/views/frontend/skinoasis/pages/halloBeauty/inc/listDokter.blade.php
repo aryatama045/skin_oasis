0ec0dd960e3da00995592658fd8121daf8389f9e
Aryatama <18211338+aryatama045@users.noreply.github.com>
2024-03-05 10:31:14 +0700

    Update listDokter.blade.php

--- resources/views/frontend/skinoasis/pages/halloBeauty/inc/listDokter.blade.php
+++ resources/views/frontend/skinoasis/pages/halloBeauty/inc/listDokter.blade.php
@@ -42,9 +42,9 @@
                             </div>
 
                             <div class="card-content text-center">
-                                <a href="#" class="btn btn-outline-yellow btn-rounded btn-sm ls-1px text-capitalize mt-4">
+                                <a href="#" class="btn btn-outline-yellow btn-rounded btn-sm ls-1px text-uppercase mt-4">
                                     {{ localize('Konsultasi Online') }}</a> <br>
-                                <a href="#" class="btn btn-outline-yellow btn-rounded btn-sm ls-1px text-capitalize mt-4">
+                                <a href="#" class="btn btn-outline-yellow btn-rounded btn-sm ls-1px text-uppercase mt-4">
                                     {{ localize('Pesan Janji Temu') }}</a>
                             </div>
 
@@ -52,43 +52,6 @@
                     </div>
                 </div>
 
-                <div class="row elements">
-                    <div class="col-xl-5col col-lg-3 col-md-3 col-6">
-                        <a href="elements-accordions.html" class="element-type">
-                            <div class="element">
-                                <img class="element-icon" src="{{ staticAsset('frontend/skinoasis/assets/images/icons/hallo-icon/cari-dokter.png') }}" alt="icon">
-                                <p>Cari Dokter</p>
-                            </div>
-                        </a>
-                    </div>
-
-                    <div class="col-xl-5col col-lg-3 col-md-3 col-6">
-                        <a href="elements-accordions.html" class="element-type">
-                            <div class="element">
-                                <img class="element-icon" src="{{ staticAsset('frontend/skinoasis/assets/images/icons/hallo-icon/list-dokter.png') }}" alt="icon">
-                                <p>List Dokter</p>
-                            </div>
-                        </a>
-                    </div>
-
-                    <div class="col-xl-5col col-lg-3 col-md-3 col-6">
-                        <a href="elements-accordions.html" class="element-type">
-                            <div class="element">
-                                <img class="element-icon" src="{{ staticAsset('frontend/skinoasis/assets/images/icons/hallo-icon/list-klinik.PNG') }}" alt="icon">
-                                <p>List Klinik</p>
-                            </div>
-                        </a>
-                    </div>
-
-                    <div class="col-xl-5col col-lg-3 col-md-3 col-6">
-                        <a href="elements-accordions.html" class="element-type">
-                            <div class="element">
-                                <img class="element-icon" src="{{ staticAsset('frontend/skinoasis/assets/images/icons/hallo-icon/paket.PNG') }}" alt="icon">
-                                <p>PAKET KECANTIKAN <br> & PERAWATAN</p>
-                            </div>
-                        </a>
-                    </div>
-                </div>
 
             </form>
 
