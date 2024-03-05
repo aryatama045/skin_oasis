d06142e8aa189037ad1140ffc251e6324e184c7a
Aryatama <18211338+aryatama045@users.noreply.github.com>
2024-03-05 10:18:49 +0700

    Update searchDokter.blade.php

--- resources/views/frontend/skinoasis/pages/halloBeauty/searchDokter.blade.php
+++ resources/views/frontend/skinoasis/pages/halloBeauty/searchDokter.blade.php
@@ -9,9 +9,13 @@
     @include('frontend.skinoasis.pages.halloBeauty.inc.1hero')
     <!--hero section end-->
 
-    <!--brand section start-->
+    <!--info section start-->
+    @include('frontend.skinoasis.pages.halloBeauty.inc.2info')
+    <!--info section end-->
+
+    <!--listdokter section start-->
     @include('frontend.skinoasis.pages.halloBeauty.inc.listDokter')
-    <!--brand section end-->
+    <!--listdokter section end-->
 
 
 @endsection
