39448dcb0d4ccb9e9dd82d88fc75574a3d2ff861
Aryatama <18211338+aryatama045@users.noreply.github.com>
2024-03-06 10:57:51 +0700

    Update janjiTemu.blade.php

--- resources/views/frontend/skinoasis/pages/halloBeauty/inc/janjiTemu.blade.php
+++ resources/views/frontend/skinoasis/pages/halloBeauty/inc/janjiTemu.blade.php
@@ -7,28 +7,28 @@
     <!-- Button Group -->
     <div class="button-group mt-3 mb-3">
         <div class="row">
-            <div class="col-sm-4 p-2">
+            <div class="col-sm-4 p-1">
                 <div class="btn-wrap">
                     <input type="radio" name="jadwal" class="btn-check" id="jadwal1" autocomplete="off">
                     <label class="btn btn-outline-yellow btn-rounded btn-sm ls-1px text-capitalize" for="jadwal1">Sabtu, 30 Des</label>
                 </div>
             </div>
 
-            <div class="col-sm-4 p-2">
+            <div class="col-sm-4 p-1">
                 <div class="btn-wrap">
                     <input type="radio" name="jadwal" class="btn-check" id="jadwal2" autocomplete="off">
                     <label class="btn btn-outline-yellow btn-rounded btn-sm ls-1px text-capitalize" for="jadwal2">Sabtu, 29 Des</label>
                 </div>
             </div>
 
-            <div class="col-sm-4 p-2">
+            <div class="col-sm-4 p-1">
                 <div class="btn-wrap">
                     <input type="radio" name="jadwal" class="btn-check" id="jadwal3" autocomplete="off">
                     <label class="btn btn-outline-yellow btn-rounded btn-sm ls-1px text-capitalize" for="jadwal3">Sabtu, 29 Des</label>
                 </div>
             </div>
 
-            <div class="col-sm-4 p-2">
+            <div class="col-sm-4 p-1">
                 <div class="btn-wrap">
                     <input type="radio" name="jadwal" class="btn-check" id="jadwal4" autocomplete="off">
                     <label class="btn btn-outline-yellow btn-rounded btn-sm ls-1px text-capitalize" for="jadwal4">Sabtu, 29 Des</label>
