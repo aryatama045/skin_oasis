4742b7e1c9021a4f283b1043ecadd56e86572966
Aryatama <18211338+aryatama045@users.noreply.github.com>
2024-03-06 09:05:29 +0700

    Update janjiTemu.blade.php

--- resources/views/frontend/skinoasis/pages/halloBeauty/inc/janjiTemu.blade.php
+++ resources/views/frontend/skinoasis/pages/halloBeauty/inc/janjiTemu.blade.php
@@ -7,55 +7,55 @@
     <!-- Button Group -->
     <div class="button-group">
         <div class="btn-wrap ">
-            <a href="#" class="btn btn-outline-yellow btn-rounded btn-sm ls-1px text-uppercase">
+            <a href="#" class="btn btn-outline-yellow btn-rounded btn-sm ls-1px text-capitalize">
                 <span>Kamis, 28 Des</span>
             </a>
         </div>
 
         <div class="btn-wrap ">
-            <a href="#" class="btn btn-outline-yellow btn-rounded btn-sm ls-1px text-uppercase">
+            <a href="#" class="btn btn-outline-yellow btn-rounded btn-sm ls-1px text-capitalize">
                 <span>Jumat, 29 Des</span>
             </a>
         </div>
 
         <div class="btn-wrap ">
-            <a href="#" class="btn btn-outline-yellow btn-rounded btn-sm ls-1px text-uppercase">
+            <a href="#" class="btn btn-outline-yellow btn-rounded btn-sm ls-1px text-capitalize">
                 <span>Sabtu, 30 Des</span>
             </a>
         </div>
 
         <div class="btn-wrap ">
-            <a href="#" class="btn btn-outline-yellow btn-rounded btn-sm ls-1px text-uppercase">
+            <a href="#" class="btn btn-outline-yellow btn-rounded btn-sm ls-1px text-capitalize">
                 <span>Kamis, 28 Des</span>
             </a>
         </div>
 
         <div class="btn-wrap ">
-            <a href="#" class="btn btn-outline-yellow btn-rounded btn-sm ls-1px text-uppercase">
+            <a href="#" class="btn btn-outline-yellow btn-rounded btn-sm ls-1px text-capitalize">
                 <span>Jumat, 29 Des</span>
             </a>
         </div>
 
         <div class="btn-wrap ">
-            <a href="#" class="btn btn-outline-yellow btn-rounded btn-sm ls-1px text-uppercase">
+            <a href="#" class="btn btn-outline-yellow btn-rounded btn-sm ls-1px text-capitalize">
                 <span>Sabtu, 30 Des</span>
             </a>
         </div>
 
         <div class="btn-wrap ">
-            <a href="#" class="btn btn-outline-yellow btn-rounded btn-sm ls-1px text-uppercase">
+            <a href="#" class="btn btn-outline-yellow btn-rounded btn-sm ls-1px text-capitalize">
                 <span>Kamis, 28 Des</span>
             </a>
         </div>
 
         <div class="btn-wrap ">
-            <a href="#" class="btn btn-outline-yellow btn-rounded btn-sm ls-1px text-uppercase">
+            <a href="#" class="btn btn-outline-yellow btn-rounded btn-sm ls-1px text-capitalize">
                 <span>Jumat, 29 Des</span>
             </a>
         </div>
 
         <div class="btn-wrap ">
-            <a href="#" class="btn btn-outline-yellow btn-rounded btn-sm ls-1px text-uppercase">
+            <a href="#" class="btn btn-outline-yellow btn-rounded btn-sm ls-1px text-capitalize">
                 <span>Sabtu, 30 Des</span>
             </a>
         </div>
