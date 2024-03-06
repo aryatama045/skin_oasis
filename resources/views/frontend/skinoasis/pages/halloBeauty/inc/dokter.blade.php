25994e08ffed0a0cef944a81de0f0eedf8495e6e
Aryatama <18211338+aryatama045@users.noreply.github.com>
2024-03-06 10:41:04 +0700

    Update dokter.blade.php

--- resources/views/frontend/skinoasis/pages/halloBeauty/inc/dokter.blade.php
+++ resources/views/frontend/skinoasis/pages/halloBeauty/inc/dokter.blade.php
@@ -6,9 +6,7 @@
                 <div class="heading text-left">
                     <div class="posts-list">
                         <figure>
-                            <a href="#">
-                                <img class="rounded-circle" src="{{ staticAsset('frontend/skinoasis/assets/images/dokter.png') }}" alt="post">
-                            </a>
+                            <img class="rounded-circle" src="{{ staticAsset('frontend/skinoasis/assets/images/dokter.png') }}" alt="post">
                         </figure>
 
                         <div>
@@ -23,14 +21,12 @@
         <div class="row">
             <div class="col-lg-7">
                 <div class="resume-dokter">
-
+                    <!-- Resume -->
                     {!! $dokter->infolain !!}
-
                 </div>
             </div>
 
             <div class="col-lg-5">
-
                 <!-- Janji Temu -->
                 @include('frontend.skinoasis.pages.halloBeauty.inc.janjiTemu')
             </div>
