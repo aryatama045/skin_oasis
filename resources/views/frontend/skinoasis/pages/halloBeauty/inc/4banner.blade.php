a48a1e7a65457e4e95d54634aa5da8d42b10b6c7
Aryatama <18211338+aryatama045@users.noreply.github.com>
2024-03-10 12:55:43 +0700

    Update 4banner.blade.php

--- resources/views/frontend/skinoasis/pages/halloBeauty/inc/4banner.blade.php
+++ resources/views/frontend/skinoasis/pages/halloBeauty/inc/4banner.blade.php
@@ -2,8 +2,13 @@
     <div class="banner-content-hallo">
         <div class="banner-subtitle">Cerita</div>
         <div class="banner-title mb-lg-15 ls-1px">#Halo Beauty</div>
-        <p class="font-weight-normal mt-3 ls-1px" style="font-size:3rem !important;">Perjuangan Hendra Melawan Jerawat Menahun.</p>
-        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. </p>
-        <a href="#" class="btn btn-rounded btn-sm text-uppercase btn-outline-green-dark ls-1">Lihat Selengkapnya</a>
     </div><!--End .banner-content-->
+
+    <div class="p-9 btn-bottom-c">
+        <div class="p-9">
+            <p class="font-weight-bold ls-1px" style="font-size:3rem !important;">Perjuangan Hendra Melawan Jerawat Menahun.</p>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. </p>
+            <a href="#" class="btn btn-rounded btn-sm text-uppercase btn-outline-green-dark ls-1">Lihat Selengkapnya</a>
+        </div>
+    </div>
 </div><!--End .banner-big-->
