9ea69d4f8a9a22803b709755b906debf24f14306
Aryatama <18211338+aryatama045@users.noreply.github.com>
2024-03-10 13:16:27 +0700

    Update 4banner.blade.php

--- resources/views/frontend/skinoasis/pages/halloBeauty/inc/4banner.blade.php
+++ resources/views/frontend/skinoasis/pages/halloBeauty/inc/4banner.blade.php
@@ -1,12 +1,12 @@
 <div class="banner-big" style="background-image: url({{ staticAsset('frontend/skinoasis/assets/images/h-b-banner.png') }}); background-color: #f1f5f7;">
     <div class="banner-content-hallo">
-        <div class="banner-subtitle">Cerita</div>
-        <div class="banner-title mb-lg-15 ls-1px">#Halo Beauty</div>
+        <div class="banner-subtitle text-black">Cerita</div>
+        <div class="banner-title ls-1px">#Halo Beauty</div>
     </div><!--End .banner-content-->
 
     <div class="p-9 btn-bottom-c">
         <div class="p-9">
-            <p class="font-weight-bold ls-1px" style="font-size:3rem !important;">Perjuangan Hendra Melawan Jerawat Menahun.</p>
+            <p class="ls-1px" style="font-size:3rem !important; font-weight: 900 !important;width: 50%;">Perjuangan Hendra Melawan Jerawat Menahun.</p>
             <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. </p>
             <a href="#" class="btn btn-rounded btn-sm text-uppercase btn-outline-green-dark ls-1">Lihat Selengkapnya</a>
         </div>
