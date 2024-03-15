caeaae1e8ff426dcde49c4e721e207ed9b0af1ce
Aryatama <18211338+aryatama045@users.noreply.github.com>
2024-03-15 08:13:29 +0700

    Update 4banner.blade.php

--- resources/views/frontend/skinoasis/pages/halloBeauty/inc/4banner.blade.php
+++ resources/views/frontend/skinoasis/pages/halloBeauty/inc/4banner.blade.php
@@ -1,15 +1,8 @@
 <div class="banner-big" style="background-image: url({{ staticAsset('frontend/skinoasis/assets/images/h-b-banner.png') }}); background-color: #f1f5f7;">
     <div class="banner-content-hallo">
-<<<<<<< HEAD
-        <div class="banner-subtitle" style="font-size:3rem !important; color: black">Cerita</div>
-        <div class="banner-title mb-lg-15 ls-1px" style="color: black;">#Halo Beauty</div>
-        <p class="mt-3 ls-1px" style="font-size:3.5rem !important; color: black; font-weight: bold;">Perjuangan Hendra <br>Melawan Jerawat <br>Menahun.</p>
-        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. </p>
-        <a href="#" class="btn btn-rounded btn-sm text-uppercase btn-outline-green-dark ls-1">Lihat Selengkapnya</a>
-=======
+
         <div class="banner-subtitle text-black">Cerita</div>
         <div class="banner-title text-black ls-1px">#Halo Beauty</div>
->>>>>>> f289eaa09fc7a525644f9a47be523753dec8a3da
     </div><!--End .banner-content-->
 
     <div class="p-9 btn-bottom-c">
