d99b5709a56900c8e79e48a77c66767ae22b8d5c
Aryatama <18211338+aryatama045@users.noreply.github.com>
2024-04-18 10:50:44 +0700

    Update ProductsController.php

--- app/Http/Controllers/Backend/Products/ProductsController.php
+++ app/Http/Controllers/Backend/Products/ProductsController.php
@@ -301,9 +301,7 @@ class ProductsController extends Controller
         $request->session()->put('stock_location_id',  $location->id);
 
         $lang_key = $request->lang_key;
-        $language = Language::where('is_active', 1)->where('code', 'en')->first();
-
-        // dd($lang_key, $language);
+        $language = Language::where('is_active', 1)->where('code', $lang_key)->first();
         if (!$language) {
             flash(localize('Language you are trying to translate is not available or not active'))->error();
             return redirect()->route('admin.products.index');
