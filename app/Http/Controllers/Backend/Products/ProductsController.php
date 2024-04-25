fa1e33293eae627047f48fdd337ef84c8d56bc1e
Aryatama <18211338+aryatama045@users.noreply.github.com>
2024-04-25 15:00:58 +0700

    Update ProductsController.php

--- app/Http/Controllers/Backend/Products/ProductsController.php
+++ app/Http/Controllers/Backend/Products/ProductsController.php
@@ -574,14 +574,14 @@ class ProductsController extends Controller
         $carts =  DB::table('carts')->select('product_variation_id')
         ->where('product_variation_id', $id_v)->get();
 
-        dd($carts);
 
-        if(!empty($carts)){
+        if($carts = TRUE){
             flash(localize('Product already is in customer cart'))->error();
 
             return back();
         }
 
+        dd('oke');
         $product->delete();
 
         flash(localize('Product has been deleted successfully'))->success();
