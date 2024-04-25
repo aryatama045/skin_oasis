3432228c73793c3ce71d3937d93088b873551acb
Aryatama <18211338+aryatama045@users.noreply.github.com>
2024-04-25 14:47:48 +0700

    Update ProductsController.php

--- app/Http/Controllers/Backend/Products/ProductsController.php
+++ app/Http/Controllers/Backend/Products/ProductsController.php
@@ -566,8 +566,10 @@ class ProductsController extends Controller
     {
         $product = Product::findOrFail($id);
 
-        $id_v = !$product->variations->isEmpty();
-        dd($id_v->product_id);
+        $variation =  DB::table('product_variations')->select('id')
+        ->where('product_id', $id)->get();
+        
+        dd($variation);
 
         $carts =  DB::table('carts')->select('product_variation_id')
         ->where('product_variation_id', $id)->get();
