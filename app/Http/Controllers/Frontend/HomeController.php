7ed2c451472da01be75fd6e7e68775e9e43faaf6
Aryatama <18211338+aryatama045@users.noreply.github.com>
2024-05-05 10:39:08 +0700

    Update HomeController.php

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -40,6 +40,7 @@ class HomeController extends Controller
             $product_cat[$key] = $val->id;
         }
 
+        dd($trending_products, $product_val);
 
         $blogs = Blog::isActive()->latest()->take(3)->get();
 
