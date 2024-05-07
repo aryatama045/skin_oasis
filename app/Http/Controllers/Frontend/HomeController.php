57f02f5c570ba98d34b980a0bddb6131626a66c8
Aryatama <18211338+aryatama045@users.noreply.github.com>
2024-05-07 16:29:09 +0700

    Update HomeController.php

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -273,7 +273,7 @@ class HomeController extends Controller
 
         $cek_mail = DB::table('partner_join')->where('email', $request->email)->get();
 
-        if (!empty($cek_mail)){
+        if ($cek_mail == TRUE){
             $mail = $cek_mail;
         }else{
             $mail = 'empty';
