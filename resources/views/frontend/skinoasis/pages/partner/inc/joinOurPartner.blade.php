e03c64b63477d6863e280d45aad58898682b31c7
Aryatama <18211338+aryatama045@users.noreply.github.com>
2024-05-07 14:16:44 +0700

    Update joinOurPartner.blade.php

--- resources/views/frontend/skinoasis/pages/partner/inc/joinOurPartner.blade.php
+++ resources/views/frontend/skinoasis/pages/partner/inc/joinOurPartner.blade.php
@@ -34,9 +34,9 @@
         <hr>
 
         <div class="col-sm-4">
-            <div class="label-input-field">
+            <div class="w-100 label-input-field">
                 <label>{{ localize('Country') }}</label>
-                <select class="form-control select2" name="country_id" class="w-100" id="country_id"
+                <select class="form-control select2 select2Address" name="country_id" class="w-100" id="country_id"
                     data-toggle="select2" data-placeholder="{{ localize('Select Country') }}" required>
                     <option value="">{{ localize('Select Country') }}</option>
                     @foreach ($country as $country)
@@ -45,6 +45,7 @@
                 </select>
 
             </div>
+
         </div>
 
         <div class="col-sm-4">
