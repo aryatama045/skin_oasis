8981588b42c8dff943e56e02223f9a0af4781960
Aryatama <18211338+aryatama045@users.noreply.github.com>
2024-05-16 09:52:38 +0700

    Update 3banners.blade.php

--- resources/views/frontend/skinoasis/pages/partials/home/3banners.blade.php
+++ resources/views/frontend/skinoasis/pages/partials/home/3banners.blade.php
@@ -19,7 +19,8 @@
                 <div class="col-md-7 p-md-0">
                     <div class="content-left" data-aos="fade-left">
                         <div class="heading mt-14">
-                            <h4 class="title mt-lg-6">Coming soon<br>Discover all your skin needs through time-efficient skin check system. Try it now!</h4>
+                            <h3 class="title mt-lg-6">Coming soon<br>
+                            <span class="h4">Discover all your skin needs through time-efficient skin check system. Try it now!</span></h3>
 
                             <!-- <p class="scan-here mt-lg-10 mb-2">
                                 Scan Here
